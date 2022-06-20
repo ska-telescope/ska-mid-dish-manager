9773703912e0602af7d90554936e053cd0c9a014
phelokazi_dube <pdube@ska.ac.za>
2022-06-20 13:20:27 +0200

    SAR-398 Removed docstrings

--- tests/dummy.py
+++ tests/dummy.py
@@ -1,3 +1,4 @@
 def test_dummy():
-    # pylint: disable=missing-module-docstring
-    """This is a dummy test function for testing"""
+    # pylint: disable=missing-function-docstring
+    pass
+    
