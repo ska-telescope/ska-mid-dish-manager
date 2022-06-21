5caa2c1888f1268e96fa1eecca9087252b31c1ea
samueltwum1 <samueltwum1@gmail.com>
2022-06-21 16:47:06 +0200

    KAR-398 Fixed linting error

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,7 @@
+# pylint: disable=missing-module-docstring
 import pytest
 
-# pylint: disable=missing-module-docstring
+
 @pytest.mark.unit
 def test_dummy():
     """This is a dummy test function for testing"""
