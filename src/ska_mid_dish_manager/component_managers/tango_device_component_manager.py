7b29184a6ad693e54fe13b8de9fe79e03a7a4d04
SKAJohanVenter <aventer@ska.ac.za>
2022-06-30 13:57:41 +0200

    KAR-400 Updated task logic. Expanded test

--- src/ska_mid_dish_manager/component_managers/tango_device_component_manager.py
+++ src/ska_mid_dish_manager/component_managers/tango_device_component_manager.py
@@ -3,14 +3,14 @@
 import logging
 import time
 from threading import Event
-from typing import AnyStr, Callable, Optional
+from typing import Any, AnyStr, Callable, Optional
 
 from ska_tango_base.base.component_manager import TaskExecutorComponentManager
 from ska_tango_base.control_model import CommunicationStatus
 from ska_tango_base.executor import TaskStatus
 from tango import DevFailed, DeviceProxy, EventData, EventType
 
-SLEEP_TIME_BETWEEN_RECONNECTS = 2  # seconds
+SLEEP_TIME_BETWEEN_RECONNECTS = 1  # seconds
 
 
 class TangoDeviceComponentManager(TaskExecutorComponentManager):
@@ -25,6 +25,7 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
     ):
         self._tango_device_fqdn = tango_device_fqdn
         self._device_proxy = None
+        self._state_subscription_id = None
         super().__init__(
             *args,
             max_workers=max_workers,
@@ -44,16 +45,10 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
         """
         Create the DeviceProxy in a thread
         """
-        task_status, response = self.submit_task(
+        self.submit_task(
             self._create_device_poxy,
             args=[self._tango_device_fqdn, self.logger],
             task_callback=self._device_proxy_creation_cb,
-        )
-        self.logger.info(
-            "Submitted _create_device_poxy task status [%s]", task_status
-        )
-        self.logger.info(
-            "Submitted _create_device_poxy task response [%s]", response
         )
 
     def _state_subscription_event_callback(self, event_data: EventData):
@@ -63,7 +58,7 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
             self.communication_state = CommunicationStatus.ESTABLISHED
 
     def _device_proxy_creation_cb(
-        self, status: TaskStatus, result: Optional[DeviceProxy] = None
+        self, status: TaskStatus, result: Any = None, retry_count=0
     ):
         """Callback to be called as _create_device_poxy runs
 
@@ -72,19 +67,35 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
         :param result: Either None or the DeviceProxy
         :type result: Optional[DeviceProxy]
         """
-        if result:
+        if status == TaskStatus.COMPLETED and isinstance(result, DeviceProxy):
             self._device_proxy = result
-            self._device_proxy.subscribe_event(
+            self._state_subscription_id = self._device_proxy.subscribe_event(
                 "State",
                 EventType.CHANGE_EVENT,
                 self._state_subscription_event_callback,
             )
 
+        if status == TaskStatus.QUEUED:
+            self.logger.info("Device Proxy creation task queued")
+
+        if status == TaskStatus.ABORTED:
+            self.logger.info("Device Proxy creation task aborted")
+
+        if status == TaskStatus.FAILED and isinstance(result, Exception):
+            self.logger.exception(result)
+            if retry_count:
+                self.logger.info("Retry count [%s]", retry_count)
+
+        if status == TaskStatus.FAILED and not isinstance(result, Exception):
+            self.logger.error("Device Proxy creation task failed [%s]", result)
+            if retry_count:
+                self.logger.info("Retry count [%s]", retry_count)
+
     @classmethod
     def _create_device_poxy(
         cls,
         tango_device_fqdn: AnyStr,
-        logger: logging.Logger,
+        _: logging.Logger,
         task_abort_event: Event = None,
         task_callback: Callable = None,
     ):
@@ -94,6 +105,7 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
 
         This method should be passed to ThreadPoolExecutor
         """
+        retry_count = 0
         while True:
 
             # If we ever need to abort this task
@@ -101,20 +113,22 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
                 task_callback(status=TaskStatus.ABORTED, result=None)
                 return
             try:
-                logger.info("Connecting to [%s]", tango_device_fqdn)
+                retry_count += 1
                 device_proxy = DeviceProxy(tango_device_fqdn)
                 task_callback(status=TaskStatus.COMPLETED, result=device_proxy)
                 return
             except DevFailed as connect_error:
-                logger.exception(connect_error)
-                logger.info(
-                    "Waiting [%s] seconds for reconnection",
-                    SLEEP_TIME_BETWEEN_RECONNECTS,
+                task_callback(
+                    status=TaskStatus.FAILED,
+                    result=connect_error,
+                    retry_count=retry_count,
                 )
                 time.sleep(SLEEP_TIME_BETWEEN_RECONNECTS)
 
     def stop_communicating(self):
-        pass
+        self.abort_tasks()
+        if self._state_subscription_id and self._device_proxy:
+            self._device_proxy.unsubscribe_event(self._state_subscription_id)
 
     def standby(self, task_callback: Callable):
         pass
--- tests/unit/test_tango_device_component_manager.py
+++ tests/unit/test_tango_device_component_manager.py
@@ -1,10 +1,23 @@
 import logging
 
+import pytest
+from ska_tango_base.control_model import CommunicationStatus
+
 from ska_mid_dish_manager.component_managers import TangoDeviceComponentManager
 
+LOGGER = logging.getLogger(__name__)
 
-def test_tango_device_component_manager():
+
+@pytest.mark.timeout(10)
+def test_tango_device_component_manager(caplog):
+    caplog.set_level(logging.INFO)
     tcmanager = TangoDeviceComponentManager(
-        "fqdn", max_workers=1, logger=logging.getLogger()
+        "fake/fqdn/1", max_workers=1, logger=LOGGER
     )
-    assert tcmanager.communication_state
+    # Keep waiting for
+    while "Retry count [3]" not in caplog.text:
+        pass
+    tcmanager.stop_communicating()
+    assert tcmanager.communication_state == CommunicationStatus.DISABLED
+    assert tcmanager._task_executor._abort_event.is_set()
+    assert tcmanager._task_executor._executor._work_queue.empty
