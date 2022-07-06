e5d23787a1ed3cb0176036877c18285de3ef9829
Katleho Madisa <katleho.madisa47@gmail.com>
2022-07-06 14:39:03 +0200

    KAR-400 Added another unit tests for the happy path
    This tests the component manager that it is able to establish communication
    when the Tango device is up and running.
    
    Co-authored-by: Samuel Twum <samueltwum1@gmail.com>, Phelokazi Dube<pdube@ska.ac.za>
    Signed-off-by: Katleho Madisa <katleho.madisa47@gmail.com>

--- tests/unit/test_tango_device_component_manager.py
+++ tests/unit/test_tango_device_component_manager.py
@@ -1,9 +1,16 @@
 import logging
+import socket
+from unittest import mock
 
 import pytest
 from ska_tango_base.control_model import CommunicationStatus
 
 from ska_mid_dish_manager.component_managers import TangoDeviceComponentManager
+
+import tango
+from tango import DevState
+from tango.server import Device
+from tango.test_context import get_host_ip, DeviceTestContext
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,3 +28,54 @@ def test_non_existing_component(caplog):
     assert (
         tc_manager.communication_state == CommunicationStatus.NOT_ESTABLISHED
     )
+
+class TestDevice(Device):
+    def init_device(self):
+        super(Device, self).init_device()
+        self.set_state(DevState.ON)
+        self.set_change_event("State", True, True)
+
+
+def _get_open_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+PORT = _get_open_port()
+
+
+@pytest.fixture
+def tango_test_context():
+    _DeviceProxy = tango.DeviceProxy
+    mock.patch(
+        'tango.DeviceProxy',
+        wraps=lambda fqdn, *args, **kwargs: _DeviceProxy(
+            "tango://{0}:{1}/{2}#dbase=no".format(get_host_ip(), PORT, fqdn),
+            *args,
+            **kwargs
+        )
+    )
+
+    with DeviceTestContext(TestDevice, port=PORT, host=get_host_ip()) as proxy:
+        yield proxy
+
+
+@pytest.mark.forked
+def test_happy_path(tango_test_context):
+    device_name = tango_test_context.name()
+    _DeviceProxy = tango.DeviceProxy
+    with mock.patch(
+        'tango.DeviceProxy',
+        wraps=lambda fqdn, *args, **kwargs: _DeviceProxy(
+            "tango://{0}:{1}/{2}#dbase=no".format(get_host_ip(), PORT, fqdn),
+            *args,
+            **kwargs
+        )
+    ):
+        tc_manager = TangoDeviceComponentManager(
+            device_name, max_workers=1, logger=LOGGER
+        )
+        tc_manager.communication_state == CommunicationStatus.ESTABLISHED
