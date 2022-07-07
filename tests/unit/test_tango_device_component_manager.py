294bc564957e92c40a7853fd1c8b14124126bc64
Katleho Madisa <katleho.madisa47@gmail.com>
2022-07-07 04:00:42 +0200

    KAR-400 Fixed up the unit tests
    In the happy_path test we had forgotten to add the assert keyword, and hence were getting
    false positive. With that change I also added time.sleep to allow the component manager's
    background thread to have enough time to update the state before we check it.
    
    In the non_existing_component test I have mock patched the DeviceProxy to get rid of the
    error about the TANGO_HOST env not set. I removed the check for CommunicationStatus == DISABLED because
    when the task is submitted, it is queued and we set the communication state to NOT_ESTABLISHED immediately. So
    
    Co-authored-by: Samuel Twum <samueltwum1@gmail.com> , Phelokazi Dube <pdube@ska.ac.za>
    Signed-off-by: Katleho Madisa <katleho.madisa47@gmail.com>

--- tests/unit/test_tango_device_component_manager.py
+++ tests/unit/test_tango_device_component_manager.py
@@ -1,33 +1,48 @@
 import logging
 import socket
+import time
 from unittest import mock
 
 import pytest
-from ska_tango_base.control_model import CommunicationStatus
-
-from ska_mid_dish_manager.component_managers import TangoDeviceComponentManager
-
 import tango
+from ska_tango_base.control_model import CommunicationStatus
 from tango import DevState
 from tango.server import Device
-from tango.test_context import get_host_ip, DeviceTestContext
+from tango.test_context import DeviceTestContext, get_host_ip
+
+from ska_mid_dish_manager.component_managers import (
+    LostConnection,
+    TangoDeviceComponentManager,
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
 @pytest.mark.timeout(10)
+@pytest.mark.forked
 def test_non_existing_component(caplog):
     caplog.set_level(logging.INFO)
-    tc_manager = TangoDeviceComponentManager(
-        "fake/fqdn/1", max_workers=1, logger=LOGGER
-    )
-    while "Connection retry count [3]" not in caplog.text:
-        pass
-    assert tc_manager.communication_state == CommunicationStatus.DISABLED
-    tc_manager.stop_communicating()
-    assert (
-        tc_manager.communication_state == CommunicationStatus.NOT_ESTABLISHED
-    )
+    _DeviceProxy = tango.DeviceProxy
+    with mock.patch(
+        "tango.DeviceProxy",
+        wraps=lambda fqdn, *args, **kwargs: _DeviceProxy(
+            "tango://{0}:{1}/{2}#dbase=no".format(get_host_ip(), PORT, fqdn),
+            *args,
+            **kwargs
+        ),
+    ):
+        tc_manager = TangoDeviceComponentManager(
+            "fake/fqdn/1", max_workers=1, logger=LOGGER
+        )
+        while "Connection retry count [3]" not in caplog.text:
+            pass
+        assert (
+            tc_manager.communication_state
+            == CommunicationStatus.NOT_ESTABLISHED
+        )
+        with pytest.raises(LostConnection):
+            tc_manager.stop_communicating()
+
 
 class TestDevice(Device):
     def init_device(self):
@@ -44,6 +59,7 @@ def _get_open_port():
     s.close()
     return port
 
+
 PORT = _get_open_port()
 
 
@@ -51,12 +67,12 @@ PORT = _get_open_port()
 def tango_test_context():
     _DeviceProxy = tango.DeviceProxy
     mock.patch(
-        'tango.DeviceProxy',
+        "tango.DeviceProxy",
         wraps=lambda fqdn, *args, **kwargs: _DeviceProxy(
             "tango://{0}:{1}/{2}#dbase=no".format(get_host_ip(), PORT, fqdn),
             *args,
             **kwargs
-        )
+        ),
     )
 
     with DeviceTestContext(TestDevice, port=PORT, host=get_host_ip()) as proxy:
@@ -68,14 +84,18 @@ def test_happy_path(tango_test_context):
     device_name = tango_test_context.name()
     _DeviceProxy = tango.DeviceProxy
     with mock.patch(
-        'tango.DeviceProxy',
+        "tango.DeviceProxy",
         wraps=lambda fqdn, *args, **kwargs: _DeviceProxy(
             "tango://{0}:{1}/{2}#dbase=no".format(get_host_ip(), PORT, fqdn),
             *args,
             **kwargs
-        )
+        ),
     ):
         tc_manager = TangoDeviceComponentManager(
             device_name, max_workers=1, logger=LOGGER
         )
-        tc_manager.communication_state == CommunicationStatus.ESTABLISHED
+
+        time.sleep(0.1)
+        assert (
+            tc_manager.communication_state == CommunicationStatus.ESTABLISHED
+        )
