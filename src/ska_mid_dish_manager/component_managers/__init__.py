5bc39b3f733ee749a8237b5a82b18ccde57678c2
Katleho Madisa <katleho.madisa47@gmail.com>
2022-07-07 04:21:56 +0200

    KAR-400 Added LostConnection import in the __init__.py
    Co-authored-by: Samuel Twum <samueltwum1@gmail.com> , Phelokazi Dube <pdube@ska.ac.za>
    Signed-off-by: Katleho Madisa <katleho.madisa47@gmail.com>

--- src/ska_mid_dish_manager/component_managers/__init__.py
+++ src/ska_mid_dish_manager/component_managers/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 # pylint: disable=C0114
 from ska_mid_dish_manager.component_managers.tango_device_component_manager import (
+    LostConnection,
     TangoDeviceComponentManager,
 )
