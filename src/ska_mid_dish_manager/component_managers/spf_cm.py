cb2855ac54f6ab002fcb00edeaf99c2b0d0a7678
SKAJohanVenter <aventer@ska.ac.za>
2022-07-20 09:08:31 +0200

    KAR-403 Added missing files

---
+++ src/ska_mid_dish_manager/component_managers/spf_cm.py
@@ -0,0 +1,41 @@
+"""Specialization for SPF functionality"""
+import logging
+from typing import AnyStr, Callable, Optional
+
+from ska_mid_dish_manager.component_managers.tango_device_cm import (
+    TangoDeviceComponentManager,
+)
+
+
+class SPFComponentManager(TangoDeviceComponentManager):
+    """Specialization for SPF functionality"""
+
+    def __init__(
+        self,
+        tango_device_fqdn: AnyStr,
+        logger: logging.Logger,
+        *args,
+        communication_state_callback: Optional[Callable] = None,
+        component_state_callback: Optional[Callable] = None,
+        **kwargs
+    ):
+        super().__init__(
+            tango_device_fqdn,
+            logger,
+            *args,
+            communication_state_callback=communication_state_callback,
+            component_state_callback=component_state_callback,
+            **kwargs
+        )
+
+    def on(self, task_callback: Callable):
+        raise NotImplementedError
+
+    def off(self, task_callback: Callable):
+        raise NotImplementedError
+
+    def reset(self, task_callback: Callable):
+        raise NotImplementedError
+
+    def standby(self, task_callback: Callable):
+        raise NotImplementedError
