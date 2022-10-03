d6ced97b8ef776fba48490072879a1d1721bb813
SKAJohanVenter <aventer@ska.ac.za>
2022-10-03 15:43:33 +0200

    KAR-463 Reverted DS device back to older version

--- src/ska_mid_dish_manager/devices/test_devices/DSDevice.py
+++ src/ska_mid_dish_manager/devices/test_devices/DSDevice.py
@@ -2,6 +2,7 @@
 # pylint: disable=invalid-name
 # pylint: disable=too-many-instance-attributes
 # pylint: disable=missing-function-docstring
+# pylint: disable=no-self-use
 # pylint: disable=protected-access
 # pylint: disable=too-many-public-methods
 # pylint: disable=attribute-defined-outside-init
@@ -10,7 +11,14 @@ import os
 import random
 import sys
 
-from tango import AttrWriteType, Database, DbDevInfo, DevShort, DevState
+from tango import (
+    AttrWriteType,
+    Database,
+    DbDevInfo,
+    DevShort,
+    DevState,
+    GreenMode,
+)
 from tango.server import Device, attribute, command
 
 from ska_mid_dish_manager.devices.test_devices.utils import (
@@ -31,6 +39,8 @@ LOGGER = logging.getLogger()
 
 class DSDevice(Device):
     """Test device for LMC"""
+
+    green_mode = GreenMode.Asyncio
 
     def init_device(self):
         super().init_device()
@@ -65,7 +75,7 @@ class DSDevice(Device):
     @attribute(
         dtype="double",
     )
-    def non_polled_attr_1(self):
+    async def non_polled_attr_1(self):
         return self.__non_polled_attr_1
 
     @attribute(
@@ -74,18 +84,18 @@ class DSDevice(Device):
         rel_change="0.5",
         abs_change="1",
     )
-    def polled_attr_1(self):
+    async def polled_attr_1(self):
         return self.__polled_attr_1
 
     @attribute(
         dtype=DSOperatingMode,
         access=AttrWriteType.READ_WRITE,
     )
-    def operatingMode(self):
+    async def operatingMode(self):
         return self._operating_mode
 
     @operatingMode.write
-    def operatingMode(self, op_mode: DSOperatingMode):
+    async def operatingMode(self, op_mode: DSOperatingMode):
         self._operating_mode = op_mode
         self.push_change_event("operatingMode", self._operating_mode)
 
@@ -93,11 +103,11 @@ class DSDevice(Device):
         dtype=PointingState,
         access=AttrWriteType.READ_WRITE,
     )
-    def pointingState(self):
+    async def pointingState(self):
         return self._pointing_state
 
     @pointingState.write
-    def pointingState(self, point_state: PointingState):
+    async def pointingState(self, point_state: PointingState):
         self._pointing_state = point_state
         self.push_change_event("pointingState", self._pointing_state)
 
@@ -105,11 +115,11 @@ class DSDevice(Device):
         dtype=HealthState,
         access=AttrWriteType.READ_WRITE,
     )
-    def healthState(self):
+    async def healthState(self):
         return self._health_state
 
     @healthState.write
-    def healthState(self, h_state: HealthState):
+    async def healthState(self, h_state: HealthState):
         self._health_state = h_state
         self.push_change_event("healthState", self._health_state)
 
@@ -117,11 +127,11 @@ class DSDevice(Device):
         dtype=DSPowerState,
         access=AttrWriteType.READ_WRITE,
     )
-    def powerState(self):
+    async def powerState(self):
         return self._power_state
 
     @powerState.write
-    def powerState(self, pwr_state: DSPowerState):
+    async def powerState(self, pwr_state: DSPowerState):
         self._power_state = pwr_state
         self.push_change_event("powerState", self._power_state)
 
@@ -129,11 +139,11 @@ class DSDevice(Device):
         dtype=IndexerPosition,
         access=AttrWriteType.READ_WRITE,
     )
-    def indexerPosition(self):
+    async def indexerPosition(self):
         return self._indexer_position
 
     @indexerPosition.write
-    def indexerPosition(self, band_number: IndexerPosition):
+    async def indexerPosition(self, band_number: IndexerPosition):
         self._indexer_position = band_number
         self.push_change_event("indexerPosition", self._indexer_position)
 
@@ -141,11 +151,11 @@ class DSDevice(Device):
         dtype=Band,
         access=AttrWriteType.READ_WRITE,
     )
-    def configuredBand(self):
+    async def configuredBand(self):
         return self._configured_band
 
     @configuredBand.write
-    def configuredBand(self, band_number: Band):
+    async def configuredBand(self, band_number: Band):
         self._configured_band = band_number
         self.push_change_event("configuredBand", self._configured_band)
 
@@ -154,11 +164,11 @@ class DSDevice(Device):
         max_dim_x=3,
         access=AttrWriteType.READ_WRITE,
     )
-    def achievedPointing(self):
+    async def achievedPointing(self):
         return self._achieved_pointing
 
     @achievedPointing.write
-    def achievedPointing(self, argin):
+    async def achievedPointing(self, argin):
         self._achieved_pointing = argin
         self.push_change_event("achievedPointing", self._achieved_pointing)
 
@@ -167,11 +177,11 @@ class DSDevice(Device):
         max_dim_x=3,
         access=AttrWriteType.READ_WRITE,
     )
-    def desiredPointing(self):
+    async def desiredPointing(self):
         return self._desired_pointing
 
     @desiredPointing.write
-    def desiredPointing(self, argin):
+    async def desiredPointing(self, argin):
         self._desired_pointing = argin
         self.push_change_event("desiredPointing", self._desired_pointing)
 
@@ -182,29 +192,29 @@ class DSDevice(Device):
     @command(
         dtype_in=None, doc_in="Update and push change event", dtype_out=None
     )
-    def IncrementNonPolled1(self):
+    async def IncrementNonPolled1(self):
         next_value = self.__non_polled_attr_1 + 1
         self.__non_polled_attr_1 = next_value
         self.push_change_event("non_polled_attr_1", next_value)
 
     @command(dtype_in=None, doc_in="Switch On", dtype_out=None)
-    def On(self):
+    async def On(self):
         self.set_state(DevState.ON)
 
     @command(dtype_in=None, doc_in="Switch Off", dtype_out=None)
-    def Off(self):
+    async def Off(self):
         self.set_state(DevState.OFF)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set StandbyLPMode", dtype_out=None)
-    def SetStandbyLPMode(self):
+    async def SetStandbyLPMode(self):
         LOGGER.info("Called SetStandbyLPMode")
         self._operating_mode = DSOperatingMode.STANDBY_LP
         self.push_change_event("operatingMode", self._operating_mode)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set StandbyFPMode", dtype_out=None)
-    def SetStandbyFPMode(self):
+    async def SetStandbyFPMode(self):
         LOGGER.info("Called SetStandbyFPMode")
         self._operating_mode = DSOperatingMode.STANDBY_FP
         self.push_change_event("operatingMode", self._operating_mode)
@@ -213,21 +223,21 @@ class DSDevice(Device):
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set Point op mode", dtype_out=None)
-    def SetPointMode(self):
+    async def SetPointMode(self):
         LOGGER.info("Called SetPointMode")
         self._operating_mode = DSOperatingMode.POINT
         self.push_change_event("operatingMode", self._operating_mode)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set SetStartupMode", dtype_out=None)
-    def SetStartupMode(self):
+    async def SetStartupMode(self):
         LOGGER.info("Called SetStartupMode")
         self._operating_mode = DSOperatingMode.STARTUP
         self.push_change_event("operatingMode", self._operating_mode)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Track", dtype_out=None)
-    def Track(self):
+    async def Track(self):
         LOGGER.info("Called Track")
         self._operating_mode = DSOperatingMode.POINT
         self.push_change_event("operatingMode", self._operating_mode)
@@ -236,21 +246,21 @@ class DSDevice(Device):
     @command(
         dtype_in=DevShort, doc_in="Update indexerPosition", dtype_out=None
     )
-    def SetIndexPosition(self, band_number):
+    async def SetIndexPosition(self, band_number):
         LOGGER.info("Called SetIndexPosition")
         self._indexer_position = IndexerPosition(band_number)
         self.push_change_event("indexerPosition", self._indexer_position)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set ConfigureBand2", dtype_out=None)
-    def ConfigureBand2(self):
+    async def ConfigureBand2(self):
         LOGGER.info("Called ConfigureBand2")
         self._configured_band = Band.B2
         self.push_change_event("configuredBand", self._configured_band)
 
     @random_delay_execution
     @command(dtype_in=None, doc_in="Set STOW", dtype_out=None)
-    def Stow(self):
+    async def Stow(self):
         LOGGER.info("Called Stow")
         self._operating_mode = DSOperatingMode.STOW
         self.push_change_event("operatingMode", self._operating_mode)
