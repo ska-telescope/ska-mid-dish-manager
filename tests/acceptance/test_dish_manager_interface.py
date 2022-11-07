fbe66fa8e26fdec10d09c8576daded182b031c8e
alethole <letholematiba@gmail.com>
2022-11-07 18:02:37 +0200

    KAR-452 Updated the SPEC_URLS

--- tests/acceptance/test_dish_manager_interface.py
+++ tests/acceptance/test_dish_manager_interface.py
@@ -5,20 +5,23 @@ from tango_simlib.utilities.validate_device import validate_device_from_url
 
 SPEC_URLS = {
     "dish_manager": (
-        "https://gitlab.com/ska-telescope/ska-telmodel/-/raw/"
-        "master/spec/tango/dsh/DishManager.yaml"
+        "https://gitlab.com/ska-telescope/ska-telmodel/-/blob/"
+        "master/tmdata/software/tango/dsh/DishManager.yaml"
+        
     ),
     "ska_controller": (
-        "https://gitlab.com/ska-telescope/ska-telmodel/-/raw/"
-        "master/spec/tango/ska_wide/SKAMaster.yaml"
+        "https://gitlab.com/ska-telescope/ska-telmodel/-/blob/"
+        "master/tmdata/software/tango/ska_wide/SKAMaster.yaml"
+        
     ),
     "ska_tango_base": (
         "https://gitlab.com/ska-telescope/ska-telmodel/-/blob/"
         "master/tmdata/software/tango/ska_wide/SKABaseDevice.yaml"
     ),
     "ska_tango_guide_ska_wide": (
-        "https://gitlab.com/ska-telescope/telescope-model/-/raw/"
-        "master/spec/tango/ska_wide/Guidelines.yaml"
+        "https://gitlab.com/ska-telescope/ska-telmodel/-/blob/"
+        "master/tmdata/software/tango/ska_wide/Guidelines.yaml"
+   
     ),
 }
 
