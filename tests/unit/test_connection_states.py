27390a7e59b3d86b3d16e5abf0316b8f5b6fdc0f
DevonPetrie <devon@sonicfactory.co.za>
2023-01-03 12:20:10 +0200

    KAR-496 Resolved linting errors

--- tests/unit/test_connection_states.py
+++ tests/unit/test_connection_states.py
@@ -1,92 +1,101 @@
 """Unit tests for subservient device connection states."""
+from unittest.mock import MagicMock, patch
+
 import pytest
 import tango
-
-from unittest.mock import MagicMock, patch
-from tango.test_context import DeviceTestContext
 from ska_tango_base.control_model import CommunicationStatus
+from tango.test_context import DeviceTestContext
+
 from ska_mid_dish_manager.devices.dish_manager import DishManager
 from ska_mid_dish_manager.models.dish_enums import DeviceConnectionState
+
 
 # pylint:disable=attribute-defined-outside-init
 @pytest.mark.unit
 @pytest.mark.forked
 class TestConnectionStates:
-  """Tests for SetOperateMode"""
-  def setup_method(self):
-      """Set up context"""
-      with patch(
-          "ska_mid_dish_manager.component_managers.tango_device_cm.tango"
-      ) as patched_tango:
-          patched_dp = MagicMock()
-          patched_dp.command_inout = MagicMock()
-          patched_tango.DeviceProxy = MagicMock(return_value=patched_dp)
-          self.tango_context = DeviceTestContext(DishManager)
-          self.tango_context.start()
+    """Tests for SetOperateMode"""
 
-  def teardown_method(self):
-      """Tear down context"""
-      self.tango_context.stop()
+    def setup_method(self):
+        """Set up context"""
+        with patch(
+            "ska_mid_dish_manager.component_managers.tango_device_cm.tango"
+        ) as patched_tango:
+            patched_dp = MagicMock()
+            patched_dp.command_inout = MagicMock()
+            patched_tango.DeviceProxy = MagicMock(return_value=patched_dp)
+            self.tango_context = DeviceTestContext(DishManager)
+            self.tango_context.start()
 
-  # pylint: disable=missing-function-docstring, protected-access
-  def test_spf_connection_state_in_sync_with_spf_cm_communication_status(
-      self,
-      event_store,
-  ):
-      device_proxy = self.tango_context.device
-      device_proxy.subscribe_event(
-          "spfConnectionState",
-          tango.EventType.CHANGE_EVENT,
-          event_store,
-      )
+    def teardown_method(self):
+        """Tear down context"""
+        self.tango_context.stop()
 
-      class_instance = DishManager.instances.get(device_proxy.name())
-      spf_cm = class_instance.component_manager.component_managers["SPF"]
+    # pylint: disable=missing-function-docstring, protected-access
+    def test_spf_connection_state_in_sync_with_spf_cm_communication_status(
+        self,
+        event_store,
+    ):
+        device_proxy = self.tango_context.device
+        device_proxy.subscribe_event(
+            "spfConnectionState",
+            tango.EventType.CHANGE_EVENT,
+            event_store,
+        )
 
-      # Force spf communication_state to NOT_ESTABLISHED
-      spf_cm._update_communication_state(communication_state=CommunicationStatus.NOT_ESTABLISHED)
+        class_instance = DishManager.instances.get(device_proxy.name())
+        spf_cm = class_instance.component_manager.component_managers["SPF"]
 
-      # We can now expect spfConnectionState to transition to DISCONNECTED
-      assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
+        # Force spf communication_state to NOT_ESTABLISHED
+        spf_cm._update_communication_state(
+            communication_state=CommunicationStatus.NOT_ESTABLISHED
+        )
 
-  # pylint: disable=missing-function-docstring, protected-access
-  def test_spfrx_connection_state_in_sync_with_spfrx_cm_communication_status(
-      self,
-      event_store,
-  ):
-      device_proxy = self.tango_context.device
-      device_proxy.subscribe_event(
-          "spfrxConnectionState",
-          tango.EventType.CHANGE_EVENT,
-          event_store,
-      )
+        # We can now expect spfConnectionState to transition to DISCONNECTED
+        assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
 
-      class_instance = DishManager.instances.get(device_proxy.name())
-      spfrx_cm = class_instance.component_manager.component_managers["SPFRX"]
+    # pylint: disable=missing-function-docstring, protected-access
+    def test_spfrx_connection_state_in_sync_with_spfrx_cm_communication_status(
+        self,
+        event_store,
+    ):
+        device_proxy = self.tango_context.device
+        device_proxy.subscribe_event(
+            "spfrxConnectionState",
+            tango.EventType.CHANGE_EVENT,
+            event_store,
+        )
 
-      # Force spfrx communication_state to NOT_ESTABLISHED
-      spfrx_cm._update_communication_state(communication_state=CommunicationStatus.NOT_ESTABLISHED)
+        class_instance = DishManager.instances.get(device_proxy.name())
+        spfrx_cm = class_instance.component_manager.component_managers["SPFRX"]
 
-      # We can now expect spfrxConnectionState to transition to DISCONNECTED
-      assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
+        # Force spfrx communication_state to NOT_ESTABLISHED
+        spfrx_cm._update_communication_state(
+            communication_state=CommunicationStatus.NOT_ESTABLISHED
+        )
 
-  # pylint: disable=missing-function-docstring, protected-access
-  def test_ds_connection_state_in_sync_with_ds_cm_communication_status(
-      self,
-      event_store,
-  ):
-      device_proxy = self.tango_context.device
-      device_proxy.subscribe_event(
-          "dsConnectionState",
-          tango.EventType.CHANGE_EVENT,
-          event_store,
-      )
+        # We can now expect spfrxConnectionState to transition to DISCONNECTED
+        assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
 
-      class_instance = DishManager.instances.get(device_proxy.name())
-      ds_cm = class_instance.component_manager.component_managers["DS"]
+    # pylint: disable=missing-function-docstring, protected-access
+    def test_ds_connection_state_in_sync_with_ds_cm_communication_status(
+        self,
+        event_store,
+    ):
+        device_proxy = self.tango_context.device
+        device_proxy.subscribe_event(
+            "dsConnectionState",
+            tango.EventType.CHANGE_EVENT,
+            event_store,
+        )
 
-      # Force ds communication_state to NOT_ESTABLISHED
-      ds_cm._update_communication_state(communication_state=CommunicationStatus.NOT_ESTABLISHED)
+        class_instance = DishManager.instances.get(device_proxy.name())
+        ds_cm = class_instance.component_manager.component_managers["DS"]
 
-      # We can now expect dsConnectionState to transition to DISCONNECTED
-      assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
+        # Force ds communication_state to NOT_ESTABLISHED
+        ds_cm._update_communication_state(
+            communication_state=CommunicationStatus.NOT_ESTABLISHED
+        )
+
+        # We can now expect dsConnectionState to transition to DISCONNECTED
+        assert event_store.wait_for_value(DeviceConnectionState.DISCONNECTED)
