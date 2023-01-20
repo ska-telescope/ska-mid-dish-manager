1042dbf055750941fb5ce2861491f3f6c2da33cb
alethole <letholematiba@gmail.com>
2023-01-20 17:23:47 +0200

    KAR-497 pylint fix
    Co-authored-by: Devon Petrie <devon@sonicfactory.co.za>
    Co-authored-by: Barbara Ojur <bojur@sarao.ac.za>
    Co-authored-by: Abednigo Lethole <letholematiba@gmail.com>

--- src/ska_mid_dish_manager/models/command_map.py
+++ src/ska_mid_dish_manager/models/command_map.py
@@ -1,8 +1,7 @@
 """Module to manage the mapping of commands to subservient devices"""
 import json
-from typing import Callable, Optional
+from typing import Callable
 
-from ska_tango_base.commands import SubmittedSlowCommand
 from ska_tango_base.executor import TaskStatus
 
 from ska_mid_dish_manager.models.dish_enums import (
