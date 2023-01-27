e7d8dad0e583085e468820c23f29cc30f8c636c3
DevonPetrie <devon@sonicfactory.co.za>
2023-01-27 15:22:12 +0200

    KAR-497 added progress checks to stow acceptance test

--- tests/acceptance/test_stow.py
+++ tests/acceptance/test_stow.py
@@ -2,33 +2,57 @@
 import pytest
 import tango
 
-from ska_mid_dish_manager.models.dish_enums import DSOperatingMode
+from ska_mid_dish_manager.models.dish_enums import DishMode, DSOperatingMode
 
 
 @pytest.mark.acceptance
 @pytest.mark.SKA_mid
 @pytest.mark.forked
-def test_stow_transition(event_store):
+def test_stow_transition(event_store_class):
     """Test transition to STOW"""
     dish_manager = tango.DeviceProxy("mid_d0001/elt/master")
     ds_device = tango.DeviceProxy("mid_d0001/lmc/ds_simulator")
     # Get at least one device into a known state
     ds_device.operatingMode = DSOperatingMode.STANDBY_FP
 
+    main_event_store = event_store_class()
+    progress_event_store = event_store_class()
+
     for attr in [
         "dishMode",
-        "longRunningCommandProgress",
         "longRunningCommandResult",
     ]:
-
         dish_manager.subscribe_event(
             attr,
             tango.EventType.CHANGE_EVENT,
-            event_store,
+            main_event_store,
         )
-    [[_], [unique_id]] = dish_manager.SetStowMode()
 
-    events = event_store.wait_for_command_id(unique_id, timeout=6)
+    dish_manager.subscribe_event(
+        "longRunningCommandProgress",
+        tango.EventType.CHANGE_EVENT,
+        progress_event_store
+    )
+
+    dish_manager.SetStowMode()
+
+    assert main_event_store.wait_for_value(DishMode.STOW, timeout=6)
+
+    expected_progress_updates = [
+        "Stow called on DS",
+        (
+            "Awaiting DS operatingmode to change to "
+            "[<DSOperatingMode.STOW: 5>]"
+        ),
+        "Awaiting dishmode change to 5",
+        ("DS operatingmode changed to, [<DSOperatingMode.STOW: 5>]"),
+        "SetStowMode completed",
+    ]
+
+    events = progress_event_store.wait_for_progress_update(
+        expected_progress_updates[-1], timeout=6
+    )
+
     events_string = "".join([str(event) for event in events])
-    for message in ["Awaiting dishmode change to 5", "SetStowMode completed"]:
+    for message in expected_progress_updates:
         assert message in events_string
