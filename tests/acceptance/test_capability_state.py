9df30a563a67dea7e2b094992e49db4f13ab3ddb
SKAJohanVenter <aventer@ska.ac.za>
2023-01-31 11:00:26 +0200

    KAR-497 Updated capability state acceptance test

--- tests/acceptance/test_capability_state.py
+++ tests/acceptance/test_capability_state.py
@@ -70,60 +70,22 @@ def test_capability_state_b2(
     """Test transition on CapabilityState b2"""
     set_dish_manager_to_standby_lp(event_store, dish_manager_proxy)
     caplog.set_level(logging.DEBUG)
-    assert dish_manager_proxy.dishMode == DishMode.STANDBY_LP
-    assert dish_manager_proxy.b2CapabilityState != CapabilityStates.CONFIGURING
 
-    ds_device_proxy.indexerPosition = IndexerPosition.MOVING
+    # If cap state is not in standby, force it
+    if dish_manager_proxy.b2CapabilityState != CapabilityStates.STANDBY:
+        spf_device_proxy.b2CapabilityState = SPFCapabilityStates.STANDBY
+        spfrx_device_proxy.b2CapabilityState = SPFRxCapabilityStates.STANDBY
 
     dish_manager_proxy.subscribe_event(
         "b2CapabilityState",
         tango.EventType.CHANGE_EVENT,
         event_store,
     )
+    event_store.wait_for_value(SPFCapabilityStates.STANDBY)
     event_store.clear_queue()
 
-    spf_event_store = EventStore()
-    spf_device_proxy.subscribe_event(
-        "b2CapabilityState",
-        tango.EventType.CHANGE_EVENT,
-        spf_event_store,
-    )
-    spf_event_store.clear_queue()
-
-    spfrx_event_store = EventStore()
-    spfrx_device_proxy.subscribe_event(
-        "b2CapabilityState",
-        tango.EventType.CHANGE_EVENT,
-        spfrx_event_store,
-    )
-    spfrx_event_store.clear_queue()
-
-    assert ds_device_proxy.indexerPosition == IndexerPosition.MOVING
-
-    if spf_device_proxy.b2CapabilityState == SPFCapabilityStates.OPERATE_FULL:
-        spf_device_proxy.b2CapabilityState = (
-            SPFCapabilityStates.OPERATE_DEGRADED
-        )
-        spf_event_store.wait_for_value(SPFCapabilityStates.OPERATE_DEGRADED)
-
-    spf_device_proxy.b2CapabilityState = SPFCapabilityStates.OPERATE_FULL
-    spf_event_store.wait_for_value(SPFCapabilityStates.OPERATE_FULL)
-
-    # Ensure updates
-    if spfrx_device_proxy.b2CapabilityState == SPFRxCapabilityStates.CONFIGURE:
-        spfrx_device_proxy.b2CapabilityState = (
-            SPFRxCapabilityStates.UNAVAILABLE
-        )
-        spfrx_event_store.wait_for_value(SPFRxCapabilityStates.UNAVAILABLE)
+    ds_device_proxy.indexerPosition = IndexerPosition.MOVING
     spfrx_device_proxy.b2CapabilityState = SPFRxCapabilityStates.CONFIGURE
-    spfrx_event_store.wait_for_value(SPFRxCapabilityStates.CONFIGURE)
-
-    assert ds_device_proxy.indexerPosition == IndexerPosition.MOVING
-    assert (
-        spf_device_proxy.b2CapabilityState == SPFCapabilityStates.OPERATE_FULL
-    )
-    assert (
-        spfrx_device_proxy.b2CapabilityState == SPFRxCapabilityStates.CONFIGURE
-    )
+    spf_device_proxy.b2CapabilityState = SPFCapabilityStates.OPERATE_FULL
 
     event_store.wait_for_value(CapabilityStates.CONFIGURING, timeout=8)
