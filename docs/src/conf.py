d92ecc60aab79147872e680aa6b3427f6982188d
DevonPetrie <devon@sonicfactory.co.za>
2023-03-27 13:16:55 +0200

    KAR-552 Prep for dish manager release 2.1.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -74,7 +74,7 @@ copyright = '2023, KAROO Team'
 author = 'KAROO Team'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
