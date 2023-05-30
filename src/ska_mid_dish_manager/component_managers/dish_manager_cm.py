b72ac84f740cb4abaee8b268e679270e9499a7fe
DevonPetrie <devon@sonicfactory.co.za>
2023-05-30 12:29:08 +0200

    KAR-580 pylint fix

--- src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
+++ src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
@@ -224,7 +224,9 @@ class DishManagerComponentManager(TaskExecutorComponentManager):
                         ds_component_state, spfrx_component_state, spf_component_state
                     )
 
-                    self._update_component_state(healthstate=new_health_state, dishmode=new_dish_mode)
+                    self._update_component_state(
+                        healthstate=new_health_state, dishmode=new_dish_mode
+                    )
                 else:
                     self._update_communication_state(CommunicationStatus.NOT_ESTABLISHED)
                     self._update_component_state(
