f0370743b504a81c919b4450597aa33d6233a33d
samueltwum1 <samueltwum1@gmail.com>
2023-05-31 12:22:34 +0100

    KAR-580 Check dish manager starting value first

--- tests/lmc-tests/acceptance/availability-and-intial-values/test_dish_lmc_default_values.py
+++ tests/lmc-tests/acceptance/availability-and-intial-values/test_dish_lmc_default_values.py
@@ -9,19 +9,6 @@ from ska_mid_dish_manager.models.dish_enums import DishMode
 
 
 @pytest.mark.lmc
-@pytest.mark.parametrize("domain", ["001"])
-@pytest.mark.parametrize("family_member", ["lmc/ds_simulator", "spf/simulator", "spfrx/simulator"])
-def test_sub_elements_startup_with_expected_operating_mode(domain, family_member):
-    """
-    Test that dish structure, spf and spfrx
-    devices report expected startup values
-    """
-    tango_device_proxy = tango.DeviceProxy(f"ska{domain}/{family_member}")
-    expected_operating_mode = "STANDBY" if "spfrx" in family_member else "STANDBY_LP"
-    assert tango_device_proxy.operatingMode.name.replace("-", "_") == expected_operating_mode
-
-
-@pytest.mark.lmc
 def test_dish_manager_startups_with_expected_dish_mode(event_store):
     """Test that dish master starts up with the expected dishMode"""
     dish_manager = tango.DeviceProxy("ska001/elt/master")
@@ -33,3 +20,17 @@ def test_dish_manager_startups_with_expected_dish_mode(event_store):
     event_store.wait_for_value(DishMode.STANDBY_LP)
     dish_mode = retrieve_attr_value(dish_manager, "dishMode")
     assert dish_mode == DishMode.STANDBY_LP.name
+
+
+@pytest.mark.lmc
+@pytest.mark.parametrize("domain", ["001"])
+@pytest.mark.parametrize("family_member", ["lmc/ds_simulator", "spf/simulator", "spfrx/simulator"])
+def test_sub_elements_startup_with_expected_operating_mode(domain, family_member):
+    """
+    Test that dish structure, spf and spfrx
+    devices report expected startup values
+    """
+    tango_device_proxy = tango.DeviceProxy(f"ska{domain}/{family_member}")
+    expected_operating_mode = "STANDBY" if "spfrx" in family_member else "STANDBY_LP"
+    assert tango_device_proxy.operatingMode.name.replace("-", "_") == expected_operating_mode
+
