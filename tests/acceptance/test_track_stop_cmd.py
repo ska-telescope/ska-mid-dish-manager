001d1a0467c61712217081987a3d9e74f6c004a8
DevonPetrie <devon@sonicfactory.co.za>
2023-06-21 13:25:55 +0200

    KAR-457 removed another unused import

--- tests/acceptance/test_track_stop_cmd.py
+++ tests/acceptance/test_track_stop_cmd.py
@@ -2,7 +2,7 @@
 import pytest
 import tango
 
-from ska_mid_dish_manager.models.dish_enums import Band, DishMode, PointingState
+from ska_mid_dish_manager.models.dish_enums import Band, DishMode
 from tests.utils import set_configuredBand_b1
 
 
