10b01639f1eddc60310f8054e866da12a989660e
samueltwum1 <samueltwum1@gmail.com>
2023-07-20 14:07:33 +0100

    KAR-606 Mark abort_commands test as xfail

--- tests/acceptance/run_last/test_abort_commands.py
+++ tests/acceptance/run_last/test_abort_commands.py
@@ -15,7 +15,7 @@ def turn_on_spf_attribute_update(request, spf_device_proxy):
     request.addfinalizer(toggle_attribute_update)
 
 
-@pytest.mark.skip("Investigate failure")
+@pytest.mark.xfail(reason="Investigate failure")
 @pytest.mark.acceptance
 @pytest.mark.SKA_mid
 @pytest.mark.forked
