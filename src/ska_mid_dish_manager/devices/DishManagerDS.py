7409817e4242eb0886988b614a0985f522659c17
SKAJohanVenter <aventer@ska.ac.za>
2023-10-20 08:53:30 +0200

    KAR-695 Bumped simulators version and fixed GetComponentStates

--- src/ska_mid_dish_manager/devices/DishManagerDS.py
+++ src/ska_mid_dish_manager/devices/DishManagerDS.py
@@ -1248,7 +1248,7 @@ class DishManager(SKAController):
         ) in self.component_manager.sub_component_managers.items():
             component_states[device] = component_state._component_state
         component_states["DM"] = self.component_manager._component_state
-        return json.dumps(component_states)
+        return json.dumps(str(component_states))
 
     @command(
         dtype_in=None,
