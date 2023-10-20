a99b99341c75e09a7fb755288995150eea133ad2
SKAJohanVenter <aventer@ska.ac.za>
2023-10-20 09:20:26 +0200

    KAR-695 Updated component states check

--- tests/unit/test_dish_manager.py
+++ tests/unit/test_dish_manager.py
@@ -132,7 +132,10 @@ class TestDishManager:
 
     def test_component_states(self):
         """Test that GetComponentStates for 3 devices are returned"""
-        assert len(json.loads(self.tango_context.device.GetComponentStates())) == 4
+        json_string = json.loads(self.tango_context.device.GetComponentStates())
+        assert "DS" in json_string
+        assert "SPFRx" in json_string
+        assert "SPF" in json_string
 
     def test_desired_pointing_write(self):
         """Test that the write method of the desiredPointing attribute functions correctly"""
