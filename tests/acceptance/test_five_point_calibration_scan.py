4cea153ff89aaf649a5e8efa2cf05421d826df09
alethole <letholematiba@gmail.com>
2023-10-23 00:52:33 +0200

    KAR-708 fix polling issue

--- tests/acceptance/test_five_point_calibration_scan.py
+++ tests/acceptance/test_five_point_calibration_scan.py
@@ -10,9 +10,10 @@ def test_program_track_table(event_store_class, dish_manager_proxy):
 
     attr_event_store = event_store_class()
     write_values = [12345, 100, 100, 123456, 101, 102]
+    dish_manager_proxy.programTrackTable = write_values
 
     dish_manager_proxy.subscribe_event(
         "programTrackTable", tango.EventType.CHANGE_EVENT, attr_event_store
     )
-    dish_manager_proxy.programTrackTable = write_values
+
     attr_event_store.wait_for_value(write_values, timeout=7)
