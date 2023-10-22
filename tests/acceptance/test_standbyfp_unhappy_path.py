6a04e6403c41475a4abdb61b8c707b890dfce1fb
alethole <letholematiba@gmail.com>
2023-10-22 23:50:14 +0200

    KAR-687 Linting fix

--- tests/acceptance/test_standbyfp_unhappy_path.py
+++ tests/acceptance/test_standbyfp_unhappy_path.py
@@ -6,21 +6,21 @@ import tango
 @pytest.mark.acceptance
 @pytest.mark.SKA_mid
 @pytest.mark.forked
-def test_standbyfpmode_raise_exceptions(dish_manager_proxy,spf_device_proxy):
+def test_standbyfpmode_raise_exceptions(event_store_class, dish_manager_proxy, spf_device_proxy):
     """Tests if standbyFP command fails when an exception is raised."""
 
     # Intentionally raising an exception on the SPF device.
     spf_device_proxy.raiseCmdException = True
 
     # Intitializing event store classes.
-    result_event_store = event_store_class()
+    progress_event_store = event_store_class()
     status_event_store = event_store_class()
 
-    # Tango Subscription to the LRC result.
+    # Tango Subscription to the LRC progress.
     dish_manager_proxy.subscribe_event(
         "longrunningCommandprogress",
         tango.EventType.CHANGE_EVENT,
-        result_event_store,
+        progress_event_store,
     )
 
     # Tango Subscription to the LRC Status.
@@ -34,12 +34,10 @@ def test_standbyfpmode_raise_exceptions(dish_manager_proxy,spf_device_proxy):
     [[_], [unique_id]] = dish_manager_proxy.SetStandbyFPMode()
     status_events = status_event_store.wait_for_command_id(unique_id, timeout=8)
 
-    expected_status = [f"{unique_id}_SetStandbyFPMode",'FAILED']
-    
+    expected_status = [f"{unique_id}_SetStandbyFPMode", "FAILED"]
+
     # Waiting for the _SetStandbyFPMode to complete executing.
-    progress_events = progress_event_store.wait_for_progress_update(
-        "SetStandbyFPMode completed", timeout=6
-    )
+    _ = progress_event_store.wait_for_progress_update("SetStandbyFPMode completed", timeout=6)
 
     # Join all the LRC status logs into one string.
     status_events_string = "".join([str(event) for event in status_events])
--- tests/acceptance/test_standbylp_unhappy_path.py
+++ tests/acceptance/test_standbylp_unhappy_path.py
@@ -8,10 +8,11 @@ from ska_mid_dish_manager.models.dish_enums import DishMode
 @pytest.mark.acceptance
 @pytest.mark.SKA_mid
 @pytest.mark.forked
-def test_standbyfpmode_raise_exceptions(dish_manager_proxy,spf_device_proxy):
+def test_standbyfpmode_raise_exceptions(event_store_class, dish_manager_proxy, spf_device_proxy):
     """Tests if standbyLP command fails when an exception is raised."""
 
-    result_event_store = event_store_class()
+    progress_event_store = event_store_class()
+    dishmode_event_store = event_store_class()
     # Tango Subscription to the dishMode attribute.
     dish_manager_proxy.subscribe_event(
         "dishMode",
@@ -28,14 +29,14 @@ def test_standbyfpmode_raise_exceptions(dish_manager_proxy,spf_device_proxy):
     spf_device_proxy.raiseCmdException = True
 
     # Intitializing event store classes.
-    result_event_store = event_store_class()
+    progress_event_store = event_store_class()
     status_event_store = event_store_class()
 
-    # Tango Subscription to the LRC result.
+    # Tango Subscription to the LRC progress.
     dish_manager_proxy.subscribe_event(
         "longrunningCommandprogress",
         tango.EventType.CHANGE_EVENT,
-        result_event_store,
+        progress_event_store,
     )
 
     # Tango Subscription to the LRC Status.
@@ -49,12 +50,10 @@ def test_standbyfpmode_raise_exceptions(dish_manager_proxy,spf_device_proxy):
     [[_], [unique_id]] = dish_manager_proxy.SetStandbyFPMode()
     status_events = status_event_store.wait_for_command_id(unique_id, timeout=8)
 
-    expected_status = [f"{unique_id}_SetStandbyLPMode",'FAILED']
-    
+    expected_status = [f"{unique_id}_SetStandbyLPMode", "FAILED"]
+
     # Waiting for the _SetStandbyLPMode to complete executing.
-    progress_events = progress_event_store.wait_for_progress_update(
-        "SetStandbyLPMode completed", timeout=6
-    )
+    _ = progress_event_store.wait_for_progress_update("SetStandbyLPMode completed", timeout=6)
 
     # Join all the LRC status logs into one string.
     status_events_string = "".join([str(event) for event in status_events])
