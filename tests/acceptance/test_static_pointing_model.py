1973c4c235bf061c2c3ce4bb3b041eee70a136d1
DevonPetrie <devon@sonicfactory.co.za>
2023-10-23 13:05:33 +0200

    KAR-707 Use dish manager proxy in test

--- tests/acceptance/test_static_pointing_model.py
+++ tests/acceptance/test_static_pointing_model.py
@@ -42,19 +42,19 @@ def test_write_band2_static_pointing_model_parameters(
 @pytest.mark.acceptance
 @pytest.mark.forked
 def test_track_load_static_off(
-    ds_manager_proxy: tango.DeviceProxy, event_store_class: Any
+    dish_manager_proxy: tango.DeviceProxy, event_store_class: Any
 ) -> None:
     """Test Band2 Static Pointing Model Parameters."""
     write_values = [20.1, 0.5]
 
-    ds_manager_proxy.TrackLoadStaticOff(json.dumps(write_values))
+    dish_manager_proxy.TrackLoadStaticOff(json.dumps(write_values))
 
     expected_values = [0.0] * 20
     expected_values[11] = write_values[0]  # CAobs
     expected_values[19] = write_values[1]  # Eobs
 
     model_event_store = event_store_class()
-    ds_manager_proxy.subscribe_event(
+    dish_manager_proxy.subscribe_event(
         "band2PointingModelParams", tango.EventType.CHANGE_EVENT, model_event_store
     )
     model_event_store.wait_for_value(expected_values, timeout=7)
