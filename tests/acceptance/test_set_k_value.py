0ae6ce559f9cd6f50ceaeb28af79bd29dbd6cced
Barbara Ojur <barbsApili@gmail.com>
2023-10-30 12:21:50 +0200

    KAR-692 Check to see if kvalue propogates to spfrx from dm in acceptance test

--- tests/acceptance/test_set_k_value.py
+++ tests/acceptance/test_set_k_value.py
@@ -7,11 +7,14 @@ import tango
 
 @pytest.mark.acceptance
 @pytest.mark.forked
-def test_set_kvalue(dish_manager_proxy: tango.DeviceProxy, event_store_class: Any) -> None:
+def test_set_kvalue(dish_manager_proxy: tango.DeviceProxy,spfrx_device_proxy: tangoDeviceProxy, event_store_class: Any) -> None:
     """Test SetKValue on SPFRx."""
     value = 5
     dish_manager_proxy.SetKValue(value)
 
-    model_event_store = event_store_class()
-    dish_manager_proxy.subscribe_event("kvalue", tango.EventType.CHANGE_EVENT, model_event_store)
-    model_event_store.wait_for_value(value, timeout=7)
+    dm_model_event_store = event_store_class()
+    spfrx_model_event_store = event_store_class()
+    dish_manager_proxy.subscribe_event("kvalue", tango.EventType.CHANGE_EVENT, dm_model_event_store)
+    spfrx_device_proxy.subscribe_event("kvalue", tango.EventType.CHANGE_EVENT, spfrx_model_event_store)
+    dm_model_event_store.wait_for_value(value, timeout=7)
+    spfrx_model_event_store.wait_for_value(value, timeout=7)
