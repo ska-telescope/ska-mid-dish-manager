6e538ed7d9ff478a6fa7ef018df717361153866e
Barbara Ojur <barbsApili@gmail.com>
2023-11-03 15:13:02 +0200

    KAR-708 XTP and bdd test update

--- tests/lmc-tests/acceptance/other/test_dish_lmc_five_point_cal_scan.py
+++ tests/lmc-tests/acceptance/other/test_dish_lmc_five_point_cal_scan.py
@@ -32,46 +32,48 @@ def test_dish_lmc_succ_of_tracking_with_program_track_table():
     pass
 
 
-@given(parse("dish_manager dishMode reports {dish_mode}"))
-def check_dish_manager_dish_mode(dish_mode, dish_manager, modes_helper):
+@given(parse("dish_manager dishMode reports OPERATE"))
+def check_dish_manager_dish_mode(dish_manager, modes_helper):
     # pylint: disable=missing-function-docstring
     # convert dish mode to have underscore
     # for DishMode OPERATE enum in utils
-    dish_mode = dish_mode.replace("-", "_")
-
+    dish_mode = "OPERATE"
     modes_helper.ensure_dish_manager_mode(dish_mode)
     current_dish_mode = retrieve_attr_value(dish_manager, "dishMode")
     LOGGER.info(f"{dish_manager} dishMode: {current_dish_mode}")
 
 
-@then(parse("dish_manager configuredBand reports B{band_number}"))
-def check_dish_configured_band(band_number, dish_manager):
+@then(parse("dish_manager configuredBand reports B2"))
+def check_dish_configured_band(dish_manager):
     # pylint: disable=missing-function-docstring
     configured_band = retrieve_attr_value(dish_manager, "configuredBand")
-    assert configured_band == f"B{band_number}"
+    assert configured_band == "B2"
     LOGGER.info(f"{dish_manager} configuredBand: {configured_band}")
 
 
 @then(
-    parse("I have issued programTrackTable on dish_manager"),
+    parse("I wrote to programTrackTable on dish_manager"),
 )
-def desired_dish_mode_program_track_table(modes_helper):
+def desired_dish_mode_program_track_table(dish_manager, modes_helper):
     # pylint: disable=missing-function-docstring
     desired_dish_mode = "programTrackTable"
+    dish_manager.programTrackTable(5)
+    assert dish_manager.programTrackTable() == 5
+
     modes_helper.dish_manager_go_to_mode(desired_dish_mode)
 
 
 @when(
-    parse("I call command {dm_command} on dish_manager"),
+    parse("I call command Track on dish_manager"),
 )
-def desired_dish_mode_dm_command(dm_command, modes_helper):
+def desired_dish_mode_dm_command(modes_helper):
     # pylint: disable=missing-function-docstring
-    desired_dish_mode = dm_command
+    desired_dish_mode = "Track"
     modes_helper.dish_manager_go_to_mode(desired_dish_mode)
 
 
-@then(parse("pointingState should report {expected_pointing_state}"))
-def check_dish_manager_pointing_state(dish_manager, event_store, expected_pointing_state):
+@then(parse("pointingState should report Track"))
+def check_dish_manager_pointing_state(dish_manager, event_store):
     # pylint: disable=missing-function-docstring
     # for cases that the event may not arrive early just wait a bit
     dish_manager.subscribe_event(
@@ -81,5 +83,29 @@ def check_dish_manager_pointing_state(dish_manager, event_store, expected_pointi
     )
 
     current_pointing_state = retrieve_attr_value(dish_manager, "pointingState")
-    assert current_pointing_state.replace("-", "_") == expected_pointing_state.replace("-", "_")
+    assert current_pointing_state.replace("-", "_") == "Track"
+    LOGGER.info(f"{dish_manager} operatingMode: {current_pointing_state}")
+
+
+@when(
+    parse("I call command TrackStop on dish_manager"),
+)
+def desired_dish_mode_dm_command(modes_helper):
+    # pylint: disable=missing-function-docstring
+    desired_dish_mode = "TrackStop"
+    modes_helper.dish_manager_go_to_mode(desired_dish_mode)
+
+
+@then(parse("pointingState should report Ready"))
+def check_dish_manager_pointing_state(dish_manager, event_store):
+    # pylint: disable=missing-function-docstring
+    # for cases that the event may not arrive early just wait a bit
+    dish_manager.subscribe_event(
+        "pointingState",
+        tango.EventType.CHANGE_EVENT,
+        event_store,
+    )
+
+    current_pointing_state = retrieve_attr_value(dish_manager, "pointingState")
+    assert current_pointing_state.replace("-", "_") == "Ready"
     LOGGER.info(f"{dish_manager} operatingMode: {current_pointing_state}")
--- tests/lmc-tests/features/XTP-28438.feature
+++ tests/lmc-tests/features/XTP-28438.feature
@@ -2,9 +2,11 @@
 Feature: Dish LMC acceptance tests
     @XTP-28438 @XTP-811  @XTP-16286
     Scenario Outline: LMC Reports on the success of Tracking with programTrackTable
-        Given dish_manager dishMode reports <dish_mode>
-        And dish_manager ConfiguredBand reports B<band_number>
-        And I have issued programTrackTable on dish_manager
+        Given dish_manager dishMode reports OPERATE
+        And dish_manager ConfiguredBand reports B2
+        And I wrote to programTrackTable on dish_manager
+        When I call command <dm_command> on dish Manager 
+        Then pointingState should report <pointing_state>
         When I call command <dm_command> on dish Manager 
         Then pointingState should report <pointing_state>
 
