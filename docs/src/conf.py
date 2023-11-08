e1ef2263baff2838a3b59825f9cd4a4446db43df
SKAJohanVenter <aventer@ska.ac.za>
2023-11-08 18:55:08 +0200

    KAR-696 Prep for release 2.3.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -74,7 +74,7 @@ copyright = '2023, KAROO Team'
 author = 'KAROO Team'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.0'
+release = '2.3.1'
 
 
 # -- General configuration ---------------------------------------------------
