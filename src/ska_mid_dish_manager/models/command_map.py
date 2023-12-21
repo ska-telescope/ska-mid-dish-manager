106b8bba3c64d52f2ddfed2b2980f6456c6d040b
DevonPetrie <devon@sonicfactory.co.za>
2023-12-21 11:51:14 +0200

    KAR-777 Ignored unused argument

--- src/ska_mid_dish_manager/models/command_map.py
+++ src/ska_mid_dish_manager/models/command_map.py
@@ -269,6 +269,7 @@ class CommandMap:
             PointingState.SLEW,
         )
 
+    # pylint: disable=unused-argument
     def scan(self, task_abort_event=None, task_callback: Optional[Callable] = None):
         """Transition the dish to Scan mode"""
         self.logger.info("Scan command called")
