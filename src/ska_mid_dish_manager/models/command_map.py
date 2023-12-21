232a41463baf5b374bda415e36fc86b3bb361eaf
DevonPetrie <devon@sonicfactory.co.za>
2023-12-21 11:45:42 +0200

    Revert "KAR-777 Implement placeholder Scan command as long running command"
    This reverts commit d30e2ffd4e3ab76d9ca90f491c69fc2f4a663282.

--- src/ska_mid_dish_manager/models/command_map.py
+++ src/ska_mid_dish_manager/models/command_map.py
@@ -269,19 +269,6 @@ class CommandMap:
             PointingState.SLEW,
         )
 
-    def scan(
-        self, task_abort_event=None, task_callback: Optional[Callable] = None
-    ):
-        """Transition the dish to Scan mode"""
-        self.logger.info(f"Scan command called")
-
-        if task_callback is not None:
-            task_callback(
-                progress=f"Scan completed",
-                status=TaskStatus.COMPLETED,
-                result=f"scan completed",
-            )
-
     def track_load_static_off(
         self, argin: list[float], task_abort_event=None, task_callback: Optional[Callable] = None
     ):
