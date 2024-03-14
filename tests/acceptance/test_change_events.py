d22d7cfddb13ea20eb56afc6aa75610efb314591
Yashren Reddi <yreddi@sarao.ac.za>
2024-03-14 18:54:44 +0200

    KAR-866 Fix linting

--- tests/acceptance/test_change_events.py
+++ tests/acceptance/test_change_events.py
@@ -6,6 +6,7 @@ import tango
 # pylint: disable=too-many-locals,unused-argument
 @pytest.mark.acceptance
 def test_attribute_events(dish_manager_proxy):
+    """Test all attributes have change events configured"""
     dm_attributes = dish_manager_proxy.get_attribute_list()
 
     all_attr_ch_events_configured = True
@@ -14,8 +15,8 @@ def test_attribute_events(dish_manager_proxy):
             dish_manager_proxy.subscribe_event(
                 attribute, tango.EventType.CHANGE_EVENT, tango.utils.EventCallback()
             )
-        except tango.DevFailed as e:
-            assert e.args[0].reason == "API_AttributePollingNotStarted"
+        except tango.DevFailed as err:
+            assert err.args[0].reason == "API_AttributePollingNotStarted"
             print(f"Attribute {attribute} does not have a push events configured.")
             all_attr_ch_events_configured = False
 
