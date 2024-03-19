453cdcad6e6274d6885c304ee6adb98ba8507e43
samueltwum1 <samueltwum1@gmail.com>
2024-03-19 15:48:08 +0000

    KAR-773 Update to minor version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -68,7 +68,7 @@ copyright = "2023, KAROO Team"
 author = "KAROO Team"
 
 # The full version, including alpha/beta/rc tags
-release = "2.3.7"
+release = "2.4.0"
 
 
 # -- General configuration ---------------------------------------------------
