1905f07f4b0db6b94fc65aabb584714fc392efe9
Barbara Ojur <barbsApili@gmail.com>
2024-04-16 10:05:21 +0200

    KAR-861 AbortCommands test update

--- tests/acceptance/run_last/test_abort_commands.py
+++ tests/acceptance/run_last/test_abort_commands.py
@@ -1,6 +1,6 @@
 """Test AbortCommands"""
+import logging
 import time
-
 import pytest
 import tango
 from ska_control_model import ResultCode
@@ -14,32 +14,35 @@ def turn_on_spf_attribute_update(request, spf_device_proxy):
 
     def toggle_attribute_update():
         spf_device_proxy.skipAttributeUpdates = False
-
     request.addfinalizer(toggle_attribute_update)
-
 
 @pytest.mark.acceptance
 @pytest.mark.SKA_mid
 @pytest.mark.forked
-def test_abort_commands(event_store, event_store_class, dish_manager_proxy, spf_device_proxy):
+def test_abort_commands(event_store_class, dish_manager_proxy, spf_device_proxy):
     """Test AbortCommands aborts the executing long running command"""
     # Set a flag on SPF to skip attribute updates
     # This is useful to ensure that the long running command
     # does not finish executing before AbortCommands is triggered
     spf_device_proxy.skipAttributeUpdates = True
-    # Create a logger
-    # logger = logging.getLogger("example_logger")
-    # logger.setLevel(logging.INFO)
+    
+    progress_event_store = event_store_class()
+    result_event_store = event_store_class()
 
-    for attr in [
-        "longRunningCommandResult",
+    # Tango subcription to LRC Progress
+    dish_manager_proxy.subscribe_event(
         "longRunningCommandProgress",
-    ]:
-        dish_manager_proxy.subscribe_event(
-            attr,
-            tango.EventType.CHANGE_EVENT,
-            event_store,
-        )
+        tango.EventType.CHANGE_EVENT,
+        progress_event_store,
+    )
+
+    # Tango subcription to LRC Result
+    dish_manager_proxy.subscribe_event(
+        "longRunningCommandResult",
+        tango.EventType.CHANGE_EVENT,
+        result_event_store,
+    )
+
 
     cmds_in_queue_store = event_store_class()
     dish_manager_proxy.subscribe_event(
@@ -48,59 +51,29 @@ def test_abort_commands(event_store, event_store_class, dish_manager_proxy, spf_
         cmds_in_queue_store,
     )
 
-    # logger.info(f": {dish_manager_proxy.dishMode}")
-
     # Transition to FP mode
     [[_], [unique_id]] = dish_manager_proxy.SetStandbyFPMode()
-    event_store.wait_for_value(
-        (f"{unique_id}", "Awaiting dishMode change to STANDBY_FP"), timeout=6
-    )
 
+    # Check that Dish Manager doesn't actually transition to FP
+    progress_event_store.wait_for_value((f"{unique_id}", "Awaiting dishMode change to STANDBY_FP"))
+    
+    # Abort the LRC
     dish_manager_proxy.AbortCommands()
 
-    time.sleep(1)
-    # logger.info(f"longRunningCommandProgress: {dish_manager_proxy.longRunningCommandProgress}")
-    assert dish_manager_proxy.longRunningCommandProgress == (
-        f"{unique_id}",
-        "SetStandbyFPMode Aborted",
-    )
-    time.sleep(1)
-    # logger.info(f"longRunningCommandProgress: {dish_manager_proxy.longRunningCommandProgress}")
-    assert dish_manager_proxy.longRunningCommandProgress == (
-        f"{unique_id}",
-        "SetStandbyFPMode Aborted",
-    )
-    time.sleep(1)
-    # logger.info(f"longRunningCommandProgress: {dish_manager_proxy.longRunningCommandProgress}")
-    assert dish_manager_proxy.longRunningCommandProgress == (
-        f"{unique_id}",
-        "SetStandbyFPMode Aborted",
-    )
-    time.sleep(1)
-    # logger.info(f"longRunningCommandProgress: {dish_manager_proxy.longRunningCommandProgress}")
-    assert dish_manager_proxy.longRunningCommandProgress == (
-        f"{unique_id}",
-        "SetStandbyFPMode Aborted",
-    )
-    time.sleep(1)
-    # logger.info(f"longRunningCommandResult: {dish_manager_proxy.longRunningCommandResult}")
+    # Confirm Dish Manager aborted the request on lRC
+    result_event_store.wait_for_command_id(unique_id, timeout=5)
+    progress_event_store.wait_for_value((f"{unique_id}", "SetStandbyFPMode Aborted"))
 
-    assert dish_manager_proxy.longRunningCommandResult == (
-        f"{unique_id}",
-        f'[{ResultCode.ABORTED}, "SetStandbyFPMode Aborted"]',
-    )
-
-    # confirm dishmanager aborted the request on lrcResult
-    # event_store.wait_for_value(
-    #     (f"{unique_id}", f"[{ResultCode.ABORTED}, SetStandbyFPMode Aborted]")
-    # )
+    # Check that the Dish Manager did not transition to FP
     assert dish_manager_proxy.dishMode != DishMode.STANDBY_FP
-    # logger.info(f": {dish_manager_proxy.dishMode}")
-    # # ensure that the queue is cleared out
-    earlier_commands_in_queue = cmds_in_queue_store.get_queue_values()
+
+    # Get earlier queue values
+    earlier_commands_in_queue = cmds_in_queue_store.get_queue_values()   
     assert earlier_commands_in_queue
 
-    # logger.info(f"Queue Values: {earlier_commands_in_queue}")
+    # Ensure that the queue is cleared out
+    while True:
+        if not dish_manager_proxy.longRunningCommandIDsInQueue:
+            assert not dish_manager_proxy.longRunningCommandIDsInQueue
+            break
 
-    # assert not dish_manager_proxy.longRunningCommandsInQueue
-    # logger.info(f"longRunningCommandsInQueue: {dish_manager_proxy.longRunningCommandsInQueue}")
