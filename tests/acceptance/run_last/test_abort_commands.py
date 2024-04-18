e9be5a56850ca34c5e9deb38352c68fd36a92855
Barbara Ojur <barbsApili@gmail.com>
2024-04-18 17:59:55 +0200

    KAR-861 increased wait times

--- tests/acceptance/run_last/test_abort_commands.py
+++ tests/acceptance/run_last/test_abort_commands.py
@@ -57,11 +57,11 @@ def test_abort_commands(event_store_class, dish_manager_proxy, spf_device_proxy)
     dish_manager_proxy.AbortCommands()
 
     # Confirm Dish Manager aborted the request on lRC
-    result_event_store.wait_for_command_id(unique_id)
+    result_event_store.wait_for_command_id(unique_id, timeout=5)
     progress_event_store.wait_for_progress_update("SetStandbyFPMode Aborted")
 
     # Check that the Dish Manager did not transition to FP
     assert dish_manager_proxy.dishMode != DishMode.STANDBY_FP
 
     # Ensure that the queue is cleared out
-    cmds_in_queue_store.wait_for_value(())
+    cmds_in_queue_store.wait_for_value((), 10)
