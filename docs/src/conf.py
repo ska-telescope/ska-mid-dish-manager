b56bc26bbcefcab28a2483089234749a6a91e02d
Devon Petrie <devonpetrie.me@gmail.com>
2024-05-17 17:14:16 +0200

    KAR-880 Increase versions to 2.6.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -68,7 +68,7 @@ copyright = "2023, KAROO Team"
 author = "KAROO Team"
 
 # The full version, including alpha/beta/rc tags
-release = "2.6.0"
+release = "2.6.1"
 
 
 # -- General configuration ---------------------------------------------------
