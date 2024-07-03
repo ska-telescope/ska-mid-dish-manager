18cfb15dbf1f2ee5618202348c7824fbb1334427
samueltwum1 <samueltwum1@gmail.com>
2024-07-03 11:34:38 +0100

    KAR-995 Remove workaround in conftest This will break the pipeline. Fix the issue afterwards

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -28,16 +28,6 @@ def setup_and_teardown(
     spfrx_device_proxy,
 ):
     """Reset the tango devices to a fresh state before each test"""
-    # FIXME weirdly, the thread spawned by the component manager hangs
-    # on initial deployment. Calling abortCommands and continuing seems
-    # to resolve this issue. Investigate why this is the case later
-    dish_manager_proxy.AbortCommands()
-    dish_manager_proxy.subscribe_event(
-        "longRunningCommandsInQueue",
-        tango.EventType.CHANGE_EVENT,
-        event_store,
-    )
-    event_store.wait_for_value((), timeout=30)
     event_store.clear_queue()
 
     spfrx_device_proxy.ResetToDefault()
