50717b33f2ef6757b72fed055a2699c21aeda8d4
samueltwum1 <samueltwum1@gmail.com>
2024-07-03 13:45:44 +0100

    KAR-995 Remove xfail tag from abort commands test

--- tests/acceptance/test_abort_commands.py
+++ tests/acceptance/test_abort_commands.py
@@ -42,7 +42,6 @@ def toggle_skip_attributes(spf_device_proxy, dish_manager_proxy, event_store_cla
 # pylint: disable=unused-argument
 @pytest.mark.acceptance
 @pytest.mark.forked
-@pytest.mark.xfail(reason="SyncComponentStates needs to be updated")
 def test_abort_commands(
     event_store_class,
     dish_manager_proxy,
