d0225f103d3c7594a7806603e6be9bb111d646e6
Yashren Reddi <yreddi@sarao.ac.za>
2024-07-16 23:40:27 +0200

    KAR-1014 Add individual scalar attributes for achieved Az and El

--- src/ska_mid_dish_manager/devices/DishManagerDS.py
+++ src/ska_mid_dish_manager/devices/DishManagerDS.py
@@ -256,11 +256,11 @@ class DishManager(SKAController):
             setattr(self, attribute_variable, comp_state_value)
             self.push_change_event(attribute_name, comp_state_value)
             self.push_archive_event(attribute_name, comp_state_value)
-            if attribute_name == "achievedPointingAzValue":
+            if attribute_name == "achievedPointingAz":
                 self._achieved_pointing_az_value = comp_state_value[1]
                 self.push_change_event("achievedPointingAzValue", comp_state_value[1])
                 self.push_archive_event("achievedPointingAzValue", comp_state_value[1])
-            if attribute_name == "achievedPointingElValue":
+            if attribute_name == "achievedPointingEl":
                 self._achieved_pointing_el_value = comp_state_value[1]
                 self.push_change_event("achievedPointingElValue", comp_state_value[1])
                 self.push_archive_event("achievedPointingElValue", comp_state_value[1])
