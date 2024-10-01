09dbc6cd53968e957121b053b8339f5e9a4ff8e3
Mpumelelo Nzama <mnzama@sarao.ac.za>
2024-10-01 19:35:33 +0200

    KAR-1173 Formated message reporting command failure

--- src/ska_mid_dish_manager/models/command_map.py
+++ src/ska_mid_dish_manager/models/command_map.py
@@ -513,7 +513,10 @@ class CommandMap:
                 task_callback(
                     progress=f"{running_command} failed",
                     status=TaskStatus.FAILED,
-                    result=(ResultCode.FAILED, f"{device} device failed executing {running_command} command with ID {self.device_command_ids[device]}"),
+                    result=(
+                        ResultCode.FAILED,
+                        f"{device} device failed executing {running_command} command with ID {self.device_command_ids[device]}",
+                    ),
                 )
 
             # Check on dishmanager to see whether the LRC has completed
