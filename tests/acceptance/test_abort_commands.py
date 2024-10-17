27a788bc7dffe19bca482c84b2e591e6bb702a60
samueltwum1 <samueltwum1@gmail.com>
2024-10-17 20:54:23 +0100

    KAR-1196 Update abort_command acceptance test

--- tests/acceptance/test_abort_commands.py
+++ tests/acceptance/test_abort_commands.py
@@ -6,40 +6,6 @@ import tango
 from ska_mid_dish_manager.models.dish_enums import DishMode, DSOperatingMode, SPFOperatingMode
 
 
-# pylint: disable=invalid-name, redefined-outer-name
-@pytest.fixture
-def toggle_skip_attributes(spf_device_proxy, dish_manager_proxy, event_store_class):
-    """Ensure that attribute updates on spf is restored"""
-    spf_device_proxy.skipAttributeUpdates = True
-    yield
-    # The test does not allow SPF to go to FP, while the others do.
-    # We need to get it back to a consistent state
-    spf_device_proxy.skipAttributeUpdates = False
-
-    dish_mode_event_store = event_store_class()
-    operating_mode_event_store = event_store_class()
-
-    spf_device_proxy.subscribe_event(
-        "operatingMode",
-        tango.EventType.CHANGE_EVENT,
-        operating_mode_event_store,
-    )
-
-    dish_manager_proxy.subscribe_event(
-        "dishMode",
-        tango.EventType.CHANGE_EVENT,
-        dish_mode_event_store,
-    )
-
-    spf_device_proxy.SetOperateMode()
-    operating_mode_event_store.wait_for_value(SPFOperatingMode.OPERATE, timeout=10)
-    # TODO DishManager does not react appropriately after it executes abort commands
-    # Investigate if further action is required on dish manager's TaskExecutor after
-    # abort is requested. DishMode reflects UNKNOWN not STANDBY_FP
-    # dish_mode_event_store.wait_for_value(DishMode.STANDBY_FP, timeout=30)
-
-
-# pylint: disable=unused-argument
 @pytest.mark.acceptance
 @pytest.mark.forked
 def test_abort_commands(
@@ -47,17 +13,19 @@ def test_abort_commands(
     dish_manager_proxy,
     spf_device_proxy,
     ds_device_proxy,
-    toggle_skip_attributes,
 ):
     """Test AbortCommands aborts the executing long running command"""
     # Set a flag on SPF to skip attribute updates.
     # This is useful to ensure that the long running command
     # does not finish executing before AbortCommands is triggered
+    spf_device_proxy.skipAttributeUpdates = True
 
+    dish_mode_event_store = event_store_class()
+    spf_operating_mode_event_store = event_store_class()
     progress_event_store = event_store_class()
     result_event_store = event_store_class()
     cmds_in_queue_store = event_store_class()
-    operating_mode_event_store = event_store_class()
+    ds_operating_mode_event_store = event_store_class()
 
     dish_manager_proxy.subscribe_event(
         "longRunningCommandResult",
@@ -77,32 +45,55 @@ def test_abort_commands(
         cmds_in_queue_store,
     )
 
+    dish_manager_proxy.subscribe_event(
+        "dishMode",
+        tango.EventType.CHANGE_EVENT,
+        dish_mode_event_store,
+    )
+
+    spf_device_proxy.subscribe_event(
+        "operatingMode",
+        tango.EventType.CHANGE_EVENT,
+        spf_operating_mode_event_store,
+    )
+
     ds_device_proxy.subscribe_event(
         "operatingMode",
         tango.EventType.CHANGE_EVENT,
-        operating_mode_event_store,
+        ds_operating_mode_event_store,
     )
 
     # Transition to FP mode
-    [[_], [unique_id]] = dish_manager_proxy.SetStandbyFPMode()
+    [[_], [fp_unique_id]] = dish_manager_proxy.SetStandbyFPMode()
 
     # wait for ds manager to finish its fanned out transition
-    operating_mode_event_store.wait_for_value(DSOperatingMode.STANDBY_FP, timeout=10)
+    ds_operating_mode_event_store.wait_for_value(DSOperatingMode.STANDBY_FP, timeout=10)
 
-    # Check that Dish Manager doesn't actually transition to FP
+    # Check that Dish Manager is waiting to transition to FP
     progress_event_store.wait_for_progress_update("Awaiting dishmode change to STANDBY_FP")
-
-    # Abort the LRC
-    dish_manager_proxy.AbortCommands()
-
-    # Confirm Dish Manager aborted the request on lRC
-    result_event_store.wait_for_command_id(unique_id)
-    progress_event_store.wait_for_progress_update("SetStandbyFPMode Aborted")
-
     # Check that the Dish Manager did not transition to FP
     assert spf_device_proxy.operatingMode == SPFOperatingMode.STANDBY_LP
     assert ds_device_proxy.operatingMode == DSOperatingMode.STANDBY_FP
     assert dish_manager_proxy.dishMode != DishMode.STANDBY_FP
 
-    # Ensure that the queue is cleared out
+    # enable spf to send attribute updates
+    spf_device_proxy.skipAttributeUpdates = False
+
+    # Abort the LRC
+    [[_], [abort_unique_id]] = dish_manager_proxy.AbortCommands()
+
+    # Confirm Dish Manager aborted the request on lRC
+    result_event_store.wait_for_command_id(fp_unique_id)
+    progress_event_store.wait_for_progress_update("SetStandbyFPMode Aborted")
+
+    # Confirm that abort finished and the queue is cleared
+    result_event_store.wait_for_command_id(abort_unique_id)
     cmds_in_queue_store.wait_for_value((), timeout=30)
+
+    # Check that the Dish Manager transitioned to FP as part of the Abort sequence
+    spf_operating_mode_event_store.wait_for_value(SPFOperatingMode.OPERATE, timeout=10)
+    dish_mode_event_store.wait_for_value(DishMode.STANDBY_FP, timeout=30)
+
+    assert spf_device_proxy.operatingMode == SPFOperatingMode.OPERATE
+    assert ds_device_proxy.operatingMode == DSOperatingMode.STANDBY_FP
+    assert dish_manager_proxy.dishMode == DishMode.STANDBY_FP
