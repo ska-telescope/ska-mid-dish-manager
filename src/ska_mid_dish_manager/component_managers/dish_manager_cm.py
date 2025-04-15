fcdd1113f48d1368fa1692d1f559581f9a3e9fca
Devon Petrie <devonpetrie.me@gmail.com>
2025-04-15 17:41:38 +0200

    KAR-1430 catch connection error

--- src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
+++ src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
@@ -259,7 +259,7 @@ class DishManagerComponentManager(TaskExecutorComponentManager):
         ds_cm = self.sub_component_managers["DS"]
         try:
             return ds_cm.execute_command("GetCurrentTAIOffset", None)
-        except tango.DevFailed:
+        except (tango.DevFailed, ConnectionError):
             self.logger.warning(
                 "Could not execute GetCurrentTAIOffset on DSManager, calculating TAI offset"
                 " manually."
