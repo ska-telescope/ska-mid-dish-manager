1e8c8c91459ed29e5379da65bc428aa311b2eee0
samueltwum1 <samueltwum1@gmail.com>
2025-05-29 15:24:20 +0100

    KAR-1408 Fetch buildState only from error event recovery The buildState refresh was reworked to be updated whenever the communication state flipped backt to ESTABLISHED from another state. That change additionally fetched the buildState every time an event was received without taking into account whether the system is in recovery from a previously failing event system or just reacting to changes on the sub device. This change ensures that only recovery states which trigger events in turn trigger a buildState update

--- src/ska_mid_dish_manager/component_managers/tango_device_cm.py
+++ src/ska_mid_dish_manager/component_managers/tango_device_cm.py
@@ -101,11 +101,10 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
             except Exception:  # pylint:disable=broad-except
                 self.logger.exception("Error occured updating component state")
 
-        # if the error event stops, tango emits a valid event for all
+        # when the error events stop, tango emits a valid event for all
         # the error events we got for the various attribute subscriptions.
         # update the communication state in case the error event callback flipped it
-        self._active_attr_event_subscriptions.add(attr_name)
-        self.sync_communication_to_valid_event()
+        self.sync_communication_to_valid_event(attr_name)
 
     def _handle_error_events(self, event_data: tango.EventData) -> None:
         """
@@ -159,12 +158,14 @@ class TangoDeviceComponentManager(TaskExecutorComponentManager):
             build_state = str(build_state)
         self._update_component_state(buildstate=build_state)
 
-    def sync_communication_to_valid_event(self) -> None:
+    def sync_communication_to_valid_event(self, event_attr_name: str) -> None:
         """Sync communication state with valid events from monitored attributes"""
-        all_monitored_events_valid = (
-            set(self._monitored_attributes) == self._active_attr_event_subscriptions
-        )
-        if all_monitored_events_valid:
+        monitored_attrs = set(self._monitored_attributes)
+        previously_synced = monitored_attrs == self._active_attr_event_subscriptions
+        self._active_attr_event_subscriptions.add(event_attr_name)
+        currently_synced = monitored_attrs == self._active_attr_event_subscriptions
+
+        if not previously_synced and currently_synced:
             self._update_communication_state(CommunicationStatus.ESTABLISHED)
             self._fetch_build_state_information()
 
