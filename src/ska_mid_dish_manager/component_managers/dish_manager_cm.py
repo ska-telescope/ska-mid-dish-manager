f24d66af0f3dca7ef8deabbc06d1eb74c483cb08
samueltwum1 <samueltwum1@gmail.com>
2025-07-03 06:54:49 +0100

    KAR-1620 Update chart config to use new wms chart variables, extend interface

--- src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
+++ src/ska_mid_dish_manager/component_managers/dish_manager_cm.py
@@ -64,6 +64,7 @@ class DishManagerComponentManager(TaskExecutorComponentManager):
         ds_device_fqdn: str,
         spf_device_fqdn: str,
         spfrx_device_fqdn: str,
+        wms_instances: List[int],
         *args,
         **kwargs,
     ):
