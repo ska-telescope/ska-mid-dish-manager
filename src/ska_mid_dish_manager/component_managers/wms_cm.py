c35670c59af5840b31a54187495cd94d1b5a18eb
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-07-03 10:44:08 +0200

    KAR-1620 Began work on non blocking start communicating logic for wms cm

--- src/ska_mid_dish_manager/component_managers/wms_cm.py
+++ src/ska_mid_dish_manager/component_managers/wms_cm.py
@@ -1,5 +1,6 @@
 """Specialization for WMS functionality."""
 
+from concurrent.futures import ThreadPoolExecutor
 import logging
 import math
 import threading
@@ -7,15 +8,15 @@ from collections import deque
 from typing import Any, Callable, Optional
 
 import tango
+import time
 from ska_control_model import AdminMode, CommunicationStatus
 from ska_tango_base.base import BaseComponentManager
+from threading import Thread
 
-GROUP_REQUEST_TIMEOUT_MS = 3000  # Determine if we want a timeout on read operations
-
+GROUP_REQUEST_TIMEOUT_MS = 3000
 
 class WMSComponentManager(BaseComponentManager):
     """Specialization for WMS functionality"""
-
     def __init__(
         self,
         logger: logging.Logger,
@@ -35,8 +36,6 @@ class WMSComponentManager(BaseComponentManager):
 
         self._wms_device_group = tango.Group("wms_devices")
 
-        self._mean_wind_speed = None
-
         # Determine the max buffer length. Once the buffer is full we will have enough data
         # points to determine the rolling average (mean wind speed)
         self._buffer_length = self._wms_instance_count * (
@@ -50,6 +49,8 @@ class WMSComponentManager(BaseComponentManager):
         self._wms_attr_polling_timer = threading.Timer(
             self._wms_polling_period, self._poll_wms_wind_speed_data
         )
+
+        self._stop_monitoring_flag = threading.Event()
 
         monitored_attr_names = ("meanwindspeed",)
         super().__init__(
@@ -65,23 +66,54 @@ class WMSComponentManager(BaseComponentManager):
     def start_communicating(self) -> None:
         """Add WMS device to group and initiate WMS attr polling."""
         self._update_communication_state(CommunicationStatus.NOT_ESTABLISHED)
+
+        if self._stop_monitoring_flag.is_set():
+            self._stop_monitoring_flag.clear()
+
         for new_wms_instance in self._wms_instances:
             wms_device_name = "mid/wms/" + str(new_wms_instance)
             self._wms_device_group.add(wms_device_name, timeout_ms=GROUP_REQUEST_TIMEOUT_MS)
 
-        try:
-            self.write_wms_group_attribute_value("adminMode", AdminMode.ONLINE)
-            self._update_communication_state(CommunicationStatus.ESTABLISHED)
-            self._poll_wms_wind_speed_data()
-        except:
-            self.logger.error(
-                "Failed to set WMS device(s) adminMode to ONLINE. One or more WMS device may be unavailable"
-            )
+        # Create a thread to recursively attempt a write to the adminMode attribute and once
+        # the write succeeds, the callback should kick off the wms data polling
+        executor = ThreadPoolExecutor(max_workers=1)
+        _wms_activated_future = executor.submit(self._activate_wms_devices)
+        _wms_activated_future.add_done_callback(self._wms_communication_established)
+
+        # try:
+        #     self.write_wms_group_attribute_value("adminMode", AdminMode.ONLINE)
+        #     self._update_communication_state(CommunicationStatus.ESTABLISHED)
+        #     self._poll_wms_wind_speed_data()
+        # except:
+        #     self.logger.error(
+        #         "Failed to set WMS device(s) adminMode to ONLINE. One or more WMS device may be unavailable"
+        #     )
+
+    def _activate_wms_devices(self) -> None:
+        # Function will recusively, within a separate thread, try to write the admin Mode to ONLINE
+        while not self._stop_monitoring_flag.is_set():
+            try:
+                self.write_wms_group_attribute_value("adminMode", AdminMode.ONLINE)
+                break
+            except:
+                self.logger.error(
+                    "Failed to set WMS device(s) adminMode to ONLINE. One or more"
+                    " WMS device(s) may be unavailable. Retrying"
+                )
+                time.sleep(1)
+
+    def _wms_communication_established(self) -> None:
+        self._update_communication_state(CommunicationStatus.ESTABLISHED)
+        self._poll_wms_wind_speed_data()
+
 
     def stop_communicating(self) -> None:
         """Stop WMS attr polling."""
         if self._wms_attr_polling_timer.is_alive():
             self._wms_attr_polling_timer.cancel()
+
+        if not self._stop_monitoring_flag.is_set():
+            self._stop_monitoring_flag.set()
 
         try:
             self.write_wms_group_attribute_value("adminMode", AdminMode.OFFLINE)
@@ -109,18 +141,18 @@ class WMSComponentManager(BaseComponentManager):
         self._compute_mean_wind_speed(wind_speed_list)
         self._restart_polling_timer()
 
+
     def _compute_mean_wind_speed(self, wind_speed_data) -> None:
         """Calculate the mean wind speed and update the component state."""
         # Push the received wind speed data points to the list
         self._ws_buffer_deque.extendleft(wind_speed_data)
-        self.logger.info(f"Computing mean windspeed. Data in buffer: {self._ws_buffer_deque}")
 
         # Ensure sufficient number of wind speed data points covering the complete
         # window period time span before computing mean wind speed
         if len(self._ws_buffer_deque) == self._buffer_length:
-            self._mean_wind_speed = sum(self._ws_buffer_deque) / self._buffer_length
-            self.logger.info(f"Computed mean wind speed: {self._mean_wind_speed}")
-            self._update_component_state(meanwindspeed=self._mean_wind_speed)
+            _mean_wind_speed = sum(self._ws_buffer_deque) / self._buffer_length
+            self.logger.info(f"Computed mean wind speed: {_mean_wind_speed}")
+            self._update_component_state(meanwindspeed=_mean_wind_speed)
 
     def read_wms_group_attribute_value(self, attribute_name: str) -> Any:
         """Return list of group attributes"""
