e396858aaf801a37826631590e2d6bfa6c4878f6
Mpumelelo Nzama <mnzama@sarao.ac.za>
2025-07-07 18:40:33 +0200

    KAR-1620 Added unit testing for wms component manager

--- tests/unit/business_logic/test_wms_cm.py
+++ tests/unit/business_logic/test_wms_cm.py
@@ -2,42 +2,48 @@
 
 import logging
 import threading
-from time import sleep
-from unittest.mock import Mock
-import pytest
+from functools import partial
+from unittest.mock import MagicMock, Mock
 
-from ska_mid_dish_manager.component_managers.wms_cm import WMSComponentManager
+import pytest
 from ska_control_model import AdminMode, CommunicationStatus
 
+from ska_mid_dish_manager.component_managers.wms_cm import WMSComponentManager
 
 WMS_POLLING_PERIOD = 1.0
 WIND_GUST_PERIOD = 3.0
 MEAN_WIND_SPEED_PERIOD = 10.0
 
 
+def comm_state_callback(signal: threading.Event, communication_state: CommunicationStatus):
+    pass
+
+
 @pytest.mark.forked
 @pytest.mark.unit
 def test_wms_group_activation_and_polling_starts():
-    test_weather_station_instances = ["1","2","3"]
+    test_weather_station_instances = ["1", "2", "3"]
     logger = logging.getLogger(__name__)
-    
+
     comp_state_updates = []
+
     def comp_state_callback(**incoming_comp_state_change):
         comp_state_updates.append(incoming_comp_state_change)
 
     wms = WMSComponentManager(
         test_weather_station_instances,
         logger=logger,
-        component_state_callback=comp_state_callback(),
+        component_state_callback=comp_state_callback,
         wms_polling_period=WMS_POLLING_PERIOD,
         wind_speed_moving_average_period=MEAN_WIND_SPEED_PERIOD,
         wind_gust_average_period=WIND_GUST_PERIOD,
     )
-    
+
     wms.write_wms_group_attribute_value = Mock()
     wms._poll_wms_wind_speed_data = Mock()
 
     wms.start_communicating()
+    wms.communication_state
 
     expected_devices = []
     for instance in test_weather_station_instances:
@@ -52,60 +58,82 @@ def test_wms_group_activation_and_polling_starts():
 
 
 @pytest.mark.unit
-def test_wms_polling_starts():
-    test_weather_station_instances = ["1","2","3"]
+def test_wms_wind_gust_and_mean_wind_speed_updates():
+    test_weather_station_instances = ["1", "2", "3"]
     logger = logging.getLogger(__name__)
-    
-    comp_state_updates = []
-    def comp_state_callback(**incoming_comp_state_change):
-        print(f"Component state callback with: {incoming_comp_state_change}")
-        comp_state_updates.append(incoming_comp_state_change)
 
-    comp_state_cb = Mock()
+    comp_state_updates = {}
+
+    def comp_state_callback(**incoming_comp_state_change):
+        print(f"Comp state callback triggered with: {incoming_comp_state_change}")
+        for key, value in incoming_comp_state_change.items():
+            comp_state_updates[key] = value
 
     wms = WMSComponentManager(
         test_weather_station_instances,
         logger=logger,
-        component_state_callback=comp_state_cb(),
+        component_state_callback=comp_state_callback,
         wms_polling_period=WMS_POLLING_PERIOD,
         wind_speed_moving_average_period=MEAN_WIND_SPEED_PERIOD,
         wind_gust_average_period=WIND_GUST_PERIOD,
+        meanwindspeed=-1,
+        windgust=-1,
     )
-    wms._wms_communication_established = Mock()
+    wait_event = threading.Event()
+    wms._communication_state_callback = partial(comm_state_callback, wait_event)
+
     wms.write_wms_group_attribute_value = Mock()
     wms.read_wms_group_attribute_value = Mock()
-
-    wms._compute_mean_wind_speed = Mock()
-    wms._process_wind_gust = Mock()
-
     wms.read_wms_group_attribute_value.return_value = [10, 20, 15]
 
-    # Call start communicating. This won't start the polling
     wms.start_communicating()
-    
-    # Now explicitly trigger the polling
-    wms._poll_wms_wind_speed_data()
-
-    # Assert that the component hasnt been updated until 
-    # enough time has elasped to update the wind gust and 
-    # mean wind speed values
-    wait_event = threading.Event()
     wait_event.wait(0.5)
+    assert wms.communication_state == CommunicationStatus.ESTABLISHED
 
-    wms.read_wms_group_attribute_value.assert_called()
-    wms._process_wind_gust.assert_called_once_with(20)
-    wms._compute_mean_wind_speed.assert_called_once_with([10, 20, 15])
+    wait_event.wait(10)
 
-    # Assert ONLY wind gust computed
-    wait_event = threading.Event()
-    wait_event.wait(5)
+    wms.stop_communicating()
+    wait_event.wait(0.5)
+    assert wms.communication_state == CommunicationStatus.DISABLED
+    wms.write_wms_group_attribute_value.assert_called_with("adminMode", AdminMode.OFFLINE)
+    assert wms.read_wms_group_attribute_value.call_count == 11
+    assert comp_state_updates["windgust"] == 20
+    assert comp_state_updates["meanwindspeed"] == 15
 
-    comp_state_cb.assert_called_with(windgust=20)
 
-    # Wait enough time for the wind gust to be computed
-    # sleep(10)
+@pytest.mark.unit
+def test_wms_wind_gust_circular_buffer():
+    test_weather_station_instances = ["1"]
+    logger = logging.getLogger(__name__)
 
-    # assert comp_state_updates
-    # print(comp_state_updates)
-    # assert False
+    comp_state_mock = MagicMock()
+
+    wms = WMSComponentManager(
+        test_weather_station_instances,
+        logger=logger,
+        component_state_callback=comp_state_mock,
+        wms_polling_period=WMS_POLLING_PERIOD,
+        wind_speed_moving_average_period=MEAN_WIND_SPEED_PERIOD,
+        wind_gust_average_period=WIND_GUST_PERIOD,
+        meanwindspeed=-1,
+        windgust=-1,
+    )
+
+    max_inst_wind_speed_and_expected_gust = [
+        (10, None),
+        (10, None),
+        (10, 10),
+        (15, 15),
+        (20, 20),
+        (30, 30),
+        (15, 30),
+        (15, 30),
+        (15, 15),
+    ]
+    for _, (current_max_wind_speed, exp_wind_gust) in enumerate(
+        max_inst_wind_speed_and_expected_gust
+    ):
+        wms._process_wind_gust(current_max_wind_speed)
+        if exp_wind_gust is not None:
+            comp_state_mock.assert_called_with(windgust=exp_wind_gust)
 
