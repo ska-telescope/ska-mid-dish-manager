6e2b6b532865e70d905fabfe3315149cac92103b
Devon Petrie <devonpetrie.me@gmail.com>
2025-07-10 13:53:28 +0200

    KAR-1588 updates

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,18 @@
 """General utils for test devices."""
 
+import math
 import queue
 import random
 import string
+import time
 from typing import Any, Callable, List, Tuple
 
 import numpy as np
 import tango
+from matplotlib import pyplot as plt
 from ska_control_model import CommunicationStatus
 
+from ska_mid_dish_manager.models.dish_enums import PointingState, TrackTableLoadMode
 from ska_mid_dish_manager.utils.ska_epoch_to_tai import get_current_tai_timestamp_from_unix_time
 
 MAX_ELEVATION = 85.0
@@ -505,3 +509,328 @@ def generate_track_table(
 
     # --- Generate the final flat track table ---
     return [val for coordinate in zip(times_tai, az_values, el_values) for val in coordinate]
+
+
+def handle_tracking_table(
+    ds_manager_proxy: tango.DeviceProxy,
+    dish_manager_proxy: tango.DeviceProxy,
+    table: list[tuple[float, float, float]],
+    start_time_tai: float,
+    pointing_tolerance_arcsec: float,
+    table_load_chunk_size: int,
+    table_load_lead_time: int,
+    event_store_class: Any,
+) -> None:
+    """Handle tracking the points from the given table.
+
+    :param ds_manager_proxy: Tango device proxy to the DSManager device
+    :type dish_manager_proxy: tango.DeviceProxy
+    :param dish_manager_proxy: Tango device proxy to the DishManager device
+    :type dish_manager_proxy: tango.DeviceProxy
+    :param table: List of (time_offset, azimuth, elevation) tuples to track.
+    :type table: list[tuple[float, float, float]]
+    :param start_time_tai: Absolute TAI time (in seconds) at which to start tracking.
+    :type start_time_tai: float
+    :param pointing_tolerance_arcsec: Tolerance (in arcsec) for pointing comparison
+    :type pointing_tolerance_arcsec: float
+    :param table_load_chunk_size: Number of points to send in each chunk.
+    :type table_load_chunk_size: int
+    :param table_load_lead_time: Lead time (in seconds) to send each chunk.
+    :type table_load_lead_time: int
+    :param event_store_class: Class used to store or observe events.
+    :type event_store_class: type
+    """
+    sub_ids = []
+    pointing_state_event_store = event_store_class()
+    sub_ids.append(
+        dish_manager_proxy.subscribe_event(
+            "pointingstate",
+            tango.EventType.CHANGE_EVENT,
+            pointing_state_event_store,
+        )
+    )
+    end_index_event_store = event_store_class()
+    sub_ids.append(
+        dish_manager_proxy.subscribe_event(
+            "trackTableEndIndex",
+            tango.EventType.CHANGE_EVENT,
+            end_index_event_store,
+        )
+    )
+    achieved_pointing_event_store = event_store_class()
+    sub_ids.append(
+        dish_manager_proxy.subscribe_event(
+            "achievedPointing",
+            tango.EventType.CHANGE_EVENT,
+            achieved_pointing_event_store,
+        )
+    )
+    achieved_target_lock_event_store = event_store_class()
+    sub_ids.append(
+        dish_manager_proxy.subscribe_event(
+            "achievedTargetLock",
+            tango.EventType.CHANGE_EVENT,
+            achieved_target_lock_event_store,
+        )
+    )
+    # Ensure the first table load is done with TrackTableLoadMode.NEW
+    dish_manager_proxy.trackTableLoadMode = TrackTableLoadMode.NEW
+
+    # Loop to load points from the csv file
+    first_load = True
+    chunk_index = 0
+    time_of_last_point_in_chunk = 0
+
+    try:
+        while True:
+            if not first_load:
+                time.sleep(0.5)
+
+                if dish_manager_proxy.pointingState not in [
+                    PointingState.TRACK,
+                    PointingState.SLEW,
+                ]:
+                    print(
+                        "Something went wrong during track, PointingState should be TRACK or SLEW"
+                        f" but got {dish_manager_proxy.pointingState.name}."
+                        f" AchievedPointing = {dish_manager_proxy.achievedpointing}."
+                        f" trackTableCurrentIndex = {dish_manager_proxy.trackTableCurrentIndex}"
+                    )
+                    return
+                # If there are still points loaded with enough lead time, then continue waiting
+                if (
+                    time_of_last_point_in_chunk - ds_manager_proxy.GetCurrentTAIOffset()
+                    > table_load_lead_time
+                ):
+                    continue
+
+            # Get the next chunk
+            chunk = table[chunk_index : chunk_index + table_load_chunk_size]
+            if not chunk:
+                # No more data to load, break and wait for the track to complete
+                break
+            chunk_index += table_load_chunk_size
+
+            # Generate the track table (adding the start time to each of the offsets)
+            chunk_track_table: list[Any] = []
+            for offset_tai, az, el in chunk:
+                chunk_track_table += [start_time_tai + offset_tai, az, el]
+            time_of_last_point_in_chunk = chunk_track_table[-3]
+
+            # Load the track table chunk
+            end_index_event_store.clear_queue()
+            dish_manager_proxy.programTrackTable = chunk_track_table
+
+            if first_load:
+                # Configure target lock with ThresholdTimePeriod of 0.1s
+                dish_manager_proxy.configureTargetLock = [pointing_tolerance_arcsec, 0.1]
+                # Start tracking and then set the track table load mode to APPEND
+                dish_manager_proxy.Track()
+                pointing_state_event_store.wait_for_value(PointingState.TRACK, timeout=30)
+                achieved_target_lock_event_store.wait_for_value(True, timeout=30)
+                dish_manager_proxy.trackTableLoadMode = TrackTableLoadMode.APPEND
+                first_load = False
+            else:
+                # TODO: PLC end index updates unexpectedly (1000 -> 1952, where 2000 was expected)
+                # expected_next_end_index = ((end_index + len(chunk) - 1) % 10000) + 1
+                # end_index_event_store.wait_for_value(expected_next_end_index, timeout=5)
+
+                # Solution until above comment is resolved
+                end_index_event_store.wait_for_n_events(1, timeout=5)
+
+        # Wait for the track to finish
+        final_tai_value = start_time_tai + table[-1][0]
+        wait_duration = final_tai_value - ds_manager_proxy.GetCurrentTAIOffset()
+        wait_duration *= 1.10  # 10% leeway
+        pointing_state_event_store.wait_for_value(PointingState.READY, timeout=wait_duration)
+    finally:
+        dish_manager_proxy.TrackStop()
+        if dish_manager_proxy.pointingState != PointingState.READY:
+            pointing_state_event_store.wait_for_value(PointingState.READY, timeout=60)
+        for sub_id in sub_ids:
+            dish_manager_proxy.unsubscribe_event(sub_id)
+
+
+def save_tracking_test_plots(
+    desired_trajectory: list[tuple[float, float, float]],
+    achieved_trajectory: list[tuple[float, float, float]],
+    err_tai_list: list[float],
+    err_angular_list: list[float],
+    save_file_path: str,
+) -> None:
+    """Save plots comparing desired and achieved pointing trajectories.
+
+    :param desired_trajectory: List of (tai, azimuth, elevation) tuples for desired points.
+    :param achieved_trajectory: List of (tai, azimuth, elevation) tuples observed during tracking.
+    :param err_tai_list: List of TAI timestamps for which errors were computed.
+    :param err_angular_list: Angular error values (arcsec) corresponding to err_tai_list.
+    :param save_file_path: Output file path for saving the plot.
+    """
+    _, ax = plt.subplots(3, 1, figsize=(18, 10), sharex=True)
+
+    # Desired
+    desired_tai_list = [tai for tai, _, _ in desired_trajectory]
+    desired_az_list = [az for _, az, _ in desired_trajectory]
+    desired_el_list = [el for _, _, el in desired_trajectory]
+
+    # Achieved
+    achieved_tai_list = [tai for tai, _, _ in achieved_trajectory]
+    achieved_az_list = [az for _, az, _ in achieved_trajectory]
+    achieved_el_list = [el for _, _, el in achieved_trajectory]
+
+    # Azimuth plot
+    ax[0].plot(desired_tai_list, desired_az_list, label="Desired Az", color="blue")
+    ax[0].plot(
+        achieved_tai_list, achieved_az_list, label="Achieved Az", color="orange", linestyle="--"
+    )
+    ax[0].set_ylabel("Azimuth (deg)")
+    ax[0].legend()
+    ax[0].grid(True)
+
+    # Elevation plot
+    ax[1].plot(desired_tai_list, desired_el_list, label="Desired El", color="blue")
+    ax[1].plot(
+        achieved_tai_list, achieved_el_list, label="Achieved El", color="orange", linestyle="--"
+    )
+    ax[1].set_ylabel("Elevation (deg)")
+    ax[1].legend()
+    ax[1].grid(True)
+
+    # Error plot
+    ax[2].plot(err_tai_list, err_angular_list, label="Angular Error", color="red")
+    ax[2].set_ylabel("Error (arcsec)")
+    ax[2].legend()
+    ax[2].grid(True)
+    ax[2].set_xlabel("TAI")
+
+    plt.tight_layout()
+    plt.savefig(save_file_path)
+
+
+def spherical_to_vector(az_value_rad: float, el_value_rad: float) -> tuple[float, float, float]:
+    """Calculates 3D vector defined by azimuth and elevation."""
+    x_value = math.cos(el_value_rad) * math.cos(az_value_rad)
+    y_value = math.cos(el_value_rad) * math.sin(az_value_rad)
+    z_value = math.sin(el_value_rad)
+    return x_value, y_value, z_value
+
+
+def calculate_on_source_dev(
+    p_desired: tuple[float, float, float], p_actual: tuple[float, float, float]
+) -> float:
+    """Calculates the angular error between the desired and actual vectors."""
+    x_desired, y_desired, z_desired = p_desired
+    x_actual, y_actual, z_actual = p_actual
+    dot_product = (x_desired * x_actual) + (y_desired * y_actual) + (z_desired * z_actual)
+    if abs(dot_product) > 1:
+        dot_product = round(dot_product, 6)
+    try:
+        err = math.acos(dot_product)
+    except ValueError:
+        print("Check vectors are normalized. arccos argument is > 1 or < -1.")
+        raise
+    return err
+
+
+def get_angular_error_between_points(az_1: float, el_1: float, az_2: float, el_2: float) -> float:
+    """Calculates the angular error between two positions."""
+    vector_1_point = spherical_to_vector(math.radians(az_1), math.radians(el_1))
+    vector_2_point = spherical_to_vector(math.radians(az_2), math.radians(el_2))
+    return calculate_on_source_dev(p_desired=vector_1_point, p_actual=vector_2_point)
+
+
+def compare_trajectories(
+    desired_trajectory: list[tuple[float, float, float]],
+    achieved_trajectory: list[tuple[float, float, float]],
+    pointing_tolerance_arcsec: float,
+) -> tuple[list[str], list[float], list[float]]:
+    """Compares desired and achieved pointing trajectories by computing interpolated errors.
+
+    :param desired_trajectory: List of (tai, azimuth, elevation) tuples representing the desired
+        path.
+    :type desired_trajectory: list[tuple[float, float, float]]
+    :param achieved_trajectory: List of (tai, azimuth, elevation) tuples representing the actual
+        path.
+    :type achieved_trajectory: list[tuple[float, float, float]]
+    :param pointing_tolerance_arcsec: Tolerance (in arcsec) for pointing comparison
+    :type pointing_tolerance_arcsec: float
+
+    :return: A tuple of:
+    - List of mismatch strings (if any).
+    - List of TAI timestamps where error was computed.
+    - List of angular errors (deg).
+    :rtype: tuple[list[str], list[float], list[float]]
+    """
+    achieved_idx = 0
+    mismatches = []
+    err_tai_list = []
+    err_angular_list = []
+    achieved_len = len(achieved_trajectory)
+    _, start_point_az, start_point_el = desired_trajectory[0]
+
+    # Loop to check that point in the desired trajectory was met
+    for desired_tai, desired_az, desired_el in desired_trajectory:
+        # If its the start point then no event would be received and we can move on.
+        # Check if the desired tai is before the first received event and if it is the start pos
+        is_tai_early = desired_tai < achieved_trajectory[0][0]
+        is_start_pos = desired_az == start_point_az and desired_el == start_point_el
+        if is_tai_early and is_start_pos:
+            continue
+
+        # AchievedPointing events may not be received at exactly the same time as the desired
+        # offsets. The below code finds the closest achieved points to the desired time and
+        # interpolates their az/el values to the desired time to get a more accurate measure of
+        # the error at that time.
+        while (
+            achieved_idx + 1 < achieved_len
+            and achieved_trajectory[achieved_idx + 1][0] < desired_tai
+        ):
+            achieved_idx += 1
+
+        if achieved_idx + 1 >= achieved_len:
+            # desired point is after the last achieved event which means the trajectory might be
+            # flat. If so, then the last received az/el event should still be within tolerance of
+            # the desired value.
+            last_az, last_el = achieved_trajectory[-1][1:3]
+            angular_err = get_angular_error_between_points(
+                desired_az, desired_el, last_az, last_el
+            )
+            is_idle = angular_err < pointing_tolerance_arcsec
+
+            if is_idle:
+                # Dish is likely idle and no more events are generated — skip this point
+                continue
+
+            mismatches.append(f"No future point for interpolation at TAI={desired_tai:.6f}")
+            continue
+
+        t0, az0, el0 = achieved_trajectory[achieved_idx]
+        t1, az1, el1 = achieved_trajectory[achieved_idx + 1]
+
+        if not (t0 <= desired_tai <= t1):
+            mismatches.append(
+                f"Desired TAI={desired_tai:.6f} out of interpolation bounds: {t0:.6f}-{t1:.6f}"
+            )
+            continue
+
+        # Interpolate
+        alpha = (desired_tai - t0) / (t1 - t0)
+        interp_az = az0 + alpha * (az1 - az0)
+        interp_el = el0 + alpha * (el1 - el0)
+
+        angular_err = get_angular_error_between_points(
+            desired_az, desired_el, interp_az, interp_el
+        )
+
+        err_tai_list.append(desired_tai)
+        err_angular_list.append(angular_err)
+
+        if angular_err > pointing_tolerance_arcsec:
+            mismatches.append(
+                f"Mismatch at TAI={desired_tai:.6f}:"
+                f" desired ({desired_az:.6f}, {desired_el:.6f}),"
+                f" interpolated ({interp_az:.6f}, {interp_el:.6f})"
+                f" err=({angular_err:.6f})"
+            )
+
+    return mismatches, err_tai_list, err_angular_list
