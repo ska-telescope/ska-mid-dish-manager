71ae38958a49ce46bc5edfeffdd41b5a58ab09d6
Yashren Reddi <yreddi@sarao.ac.za>
2025-08-11 10:16:43 +0200

    KAR-1710 Fix maintenance cmd test

--- tests/acceptance/test_maintenance_mode.py
+++ tests/acceptance/test_maintenance_mode.py
@@ -10,12 +10,13 @@ from ska_mid_dish_manager.models.dish_enums import (
     SPFOperatingMode,
     SPFRxOperatingMode,
 )
-from tests.utils import EventStore, setup_subscriptions
+from tests.utils import EventStore, remove_subscriptions, setup_subscriptions
 
 
 @pytest.mark.acceptance
 @pytest.mark.forked
 def test_maintenance_mode_cmds(
+    reset_dish_to_standby: any,
     event_store_class: EventStore,
     dish_manager_proxy: DeviceProxy,
     ds_device_proxy: DeviceProxy,
@@ -29,32 +30,24 @@ def test_maintenance_mode_cmds(
     spf_event_store = event_store_class()
     spfrx_event_store = event_store_class()
 
-    subscriptions = []
-    subscriptions.append(setup_subscriptions(dish_manager_proxy, {"dishMode": mode_event_store}))
-    ds_attr_cb_mapping = {
-        "dsccmdAuth": dsc_event_store,
-        "operatingmode": dsc_auth_event_store,
-    }
-    subscriptions.append(setup_subscriptions(ds_device_proxy, ds_attr_cb_mapping))
-    subscriptions.append(setup_subscriptions(spf_device_proxy, {"operatingmode": spf_event_store}))
-    subscriptions.append(
-        setup_subscriptions(spfrx_device_proxy, {"operatingmode": spfrx_event_store})
+    subscriptions = {}
+    subscriptions.update(setup_subscriptions(spf_device_proxy, {"operatingMode": spf_event_store}))
+    subscriptions.update(
+        setup_subscriptions(spfrx_device_proxy, {"operatingMode": spfrx_event_store})
     )
-
-    mode_event_store.clear_queue()
-    dsc_auth_event_store.clear_queue()
-    spf_event_store.clear_queue()
-    spfrx_event_store.clear_queue()
-    dsc_event_store.clear_queue()
-    dish_manager_proxy.SetStowMode()
+    ds_attr_cb_mapping = {
+        "dscCmdAuth": dsc_auth_event_store,
+        "operatingMode": dsc_event_store,
+    }
+    subscriptions.update(setup_subscriptions(ds_device_proxy, ds_attr_cb_mapping))
+    subscriptions.update(setup_subscriptions(dish_manager_proxy, {"dishMode": mode_event_store}))
 
     dish_manager_proxy.SetMaintenanceMode()
 
-    dsc_event_store.wait_for_value(DSOperatingMode.STOW, timeout=60)
+    dsc_event_store.wait_for_value(DSOperatingMode.STOW, timeout=120)
     spfrx_event_store.wait_for_value(SPFRxOperatingMode.STANDBY, timeout=30)
     spf_event_store.wait_for_value(SPFOperatingMode.MAINTENANCE, timeout=30)
-    mode_event_store.wait_for_value(DishMode.Maintenance, timeout=30)
-    dsc_auth_event_store.wait_for_value(DscCmdAuthType.NO_AUTHORITHY, timeout=30)
+    mode_event_store.wait_for_value(DishMode.MAINTENANCE, timeout=30)
+    dsc_auth_event_store.wait_for_value(DscCmdAuthType.NO_AUTHORITY, timeout=30)
 
-    for subscription in subscriptions:
-        subscription.remove()
+    remove_subscriptions(subscriptions)
