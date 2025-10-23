87ff9d68e9d216700f18951a3365ff08fd12bb7e
Devon Petrie <devonpetrie.me@gmail.com>
2025-10-23 11:48:42 +0200

    KAR-1738 increase timeout for dsmanager reset

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -111,7 +111,7 @@ def reset_dish_to_standby(
             ):
                 # go to LP ...
                 ds_device_proxy.SetStandbyMode()
-            dish_mode_events.wait_for_value(DishMode.STANDBY_LP, timeout=10)
+            dish_mode_events.wait_for_value(DishMode.STANDBY_LP, timeout=20)
     except (RuntimeError, AssertionError):
         # check dish manager before giving up
         logger.exception("Failed to reset subdevices to known states.")
