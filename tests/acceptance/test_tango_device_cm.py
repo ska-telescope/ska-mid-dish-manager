2204d0cba066272f03e2da7649d5a9fe07caf41c
Yashren Reddi <yreddi@sarao.ac.za>
2025-11-07 14:03:17 +0200

    KAR-1780 Cleanup resources in tes

--- tests/acceptance/test_tango_device_cm.py
+++ tests/acceptance/test_tango_device_cm.py
@@ -34,22 +34,22 @@ def test_tango_device_component_manager_state(
         communication_state_callback=mock_callable,
     )
     assert com_man.communication_state == CommunicationStatus.DISABLED
+    try:
+        com_man.start_communicating()
+        assert com_man.communication_state == CommunicationStatus.NOT_ESTABLISHED
 
-    com_man.start_communicating()
-    assert com_man.communication_state == CommunicationStatus.NOT_ESTABLISHED
+        mock_callable.assert_call(CommunicationStatus.ESTABLISHED, lookahead=3)
 
-    mock_callable.assert_call(CommunicationStatus.ESTABLISHED, lookahead=3)
+        assert com_man.communication_state == CommunicationStatus.ESTABLISHED
 
-    assert com_man.communication_state == CommunicationStatus.ESTABLISHED
-
-    test_mode_initial_val = device_proxy.read_attribute("testmode").value
-    new_value = 0 if test_mode_initial_val else 1
-    device_proxy.testmode = new_value
-    assert component_state_store.wait_for_value("testmode", new_value, timeout=7)
-    device_proxy.testmode = test_mode_initial_val
-
-    com_man.stop_communicating()
-    assert com_man.communication_state == CommunicationStatus.DISABLED
+        test_mode_initial_val = device_proxy.read_attribute("testmode").value
+        new_value = 0 if test_mode_initial_val else 1
+        device_proxy.testmode = new_value
+        assert component_state_store.wait_for_value("testmode", new_value, timeout=7)
+        device_proxy.testmode = test_mode_initial_val
+    finally:
+        com_man.stop_communicating()
+        assert com_man.communication_state == CommunicationStatus.DISABLED
 
 
 @pytest.mark.acceptance
@@ -65,18 +65,21 @@ def test_stress_component_monitor(monitor_tango_servers, component_state_store, 
         communication_state_callback=mock_callable,
     )
 
-    com_man.start_communicating()
+    try:
+        com_man.start_communicating()
 
-    mock_callable.assert_call(CommunicationStatus.ESTABLISHED, lookahead=3)
+        mock_callable.assert_call(CommunicationStatus.ESTABLISHED, lookahead=3)
 
-    device_proxy = tango.DeviceProxy(ds_device_fqdn)
-    # testMode is not polled
-    device_proxy.poll_attribute("testMode", 100)
+        device_proxy = tango.DeviceProxy(ds_device_fqdn)
+        # testMode is not polled
+        device_proxy.poll_attribute("testMode", 100)
 
-    test_mode_initial_val = device_proxy.read_attribute("testmode").value
-    for _ in range(10):
-        current_val = device_proxy.read_attribute("testmode").value
-        new_val = 0 if current_val else 1
-        device_proxy.testmode = new_val
-        assert component_state_store.wait_for_value("testmode", new_val)
-    device_proxy.testmode = test_mode_initial_val
+        test_mode_initial_val = device_proxy.read_attribute("testmode").value
+        for _ in range(10):
+            current_val = device_proxy.read_attribute("testmode").value
+            new_val = 0 if current_val else 1
+            device_proxy.testmode = new_val
+            assert component_state_store.wait_for_value("testmode", new_val)
+        device_proxy.testmode = test_mode_initial_val
+    finally:
+        com_man.stop_communicating()
