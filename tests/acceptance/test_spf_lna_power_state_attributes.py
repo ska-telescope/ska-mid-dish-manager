0680f0fc46f034acab12f3f93e751fda6aaffa4c
silere8 <usilere@sarao.ac.za>
2025-12-24 09:59:33 +0200

    KAR-1934 Add try to try address the empty event queue error

--- tests/acceptance/test_spf_lna_power_state_attributes.py
+++ tests/acceptance/test_spf_lna_power_state_attributes.py
@@ -70,35 +70,43 @@ def test_spf_lna_power_state_rejects_attribute_writes(
 
 @pytest.mark.acceptance
 @pytest.mark.parametrize(
-    "attribute_name",
+    "band, attribute_name",
     [
-        "b1lnahpowerstate",
-        "b2lnahpowerstate",
-        "b3lnahpowerstate",
-        "b4lnahpowerstate",
-        "b5alnahpowerstate",
-        "b5blnahpowerstate",
+        ("1", "b1lnahpowerstate"),
+        ("2", "b2lnahpowerstate"),
+        ("3", "b3lnahpowerstate"),
+        ("4", "b4lnahpowerstate"),
+        ("5a", "b5alnahpowerstate"),
+        ("5b", "b5blnahpowerstate"),
     ],
 )
 def test_spf_lna_power_state_change_on_dishmode_operate(
+    band: str,
     attribute_name: str,
     dish_manager_proxy: tango.DeviceProxy,
     event_store_class: Any,
 ) -> None:
     """Test that SPF Lna Power State change updates when dishmode is in operate."""
     dm_event_store = event_store_class()
-    spf_lna_power_state_attr_event_store = event_store_class()
+    spf_lna_event_store = event_store_class()
     attr_cb_mapping = {
         "dishMode": dm_event_store,
-        attribute_name: spf_lna_power_state_attr_event_store,
+        attribute_name: spf_lna_event_store,
     }
+
     subscriptions = setup_subscriptions(dish_manager_proxy, attr_cb_mapping)
-    if dish_manager_proxy.dishmode != DishMode.OPERATE:
-        dish_manager_proxy.ConfigureBand1(True)
-    dm_event_store.wait_for_value(DishMode.OPERATE, timeout=30)
-    dish_manager_proxy.write_attribute(attribute_name, True)
-    spf_lna_power_state_attr_event_store.wait_for_value(True, timeout=30)
-    remove_subscriptions(subscriptions)
+
+    try:
+        if dish_manager_proxy.dishMode != DishMode.OPERATE:
+            configure_band_cmd = getattr(dish_manager_proxy, f"ConfigureBand{band}")
+            configure_band_cmd(True)
+
+        dm_event_store.wait_for_value(DishMode.OPERATE, timeout=60)
+        dish_manager_proxy.write_attribute(attribute_name, True)
+        spf_lna_event_store.wait_for_value(True, timeout=30, proxy=dish_manager_proxy)
+
+    finally:
+        remove_subscriptions(subscriptions)
 
 
 @pytest.mark.acceptance
@@ -127,9 +135,11 @@ def test_spf_lna_power_state_change_on_dishmode_maintainance(
         attribute_name: spf_lna_power_state_attr_event_store,
     }
     subscriptions = setup_subscriptions(dish_manager_proxy, attr_cb_mapping)
-    if dish_manager_proxy.dishmode != DishMode.MAINTENANCE:
-        dish_manager_proxy.SetMaintenanceMode()
-    dm_event_store.wait_for_value(DishMode.MAINTENANCE, timeout=90)
-    dish_manager_proxy.write_attribute(attribute_name, True)
-    spf_lna_power_state_attr_event_store.wait_for_value(True, timeout=30)
-    remove_subscriptions(subscriptions)
+    try:
+        if dish_manager_proxy.dishmode != DishMode.MAINTENANCE:
+            dish_manager_proxy.SetMaintenanceMode()
+        dm_event_store.wait_for_value(DishMode.MAINTENANCE, timeout=90)
+        dish_manager_proxy.write_attribute(attribute_name, True)
+        spf_lna_power_state_attr_event_store.wait_for_value(True, timeout=30)
+    finally:
+        remove_subscriptions(subscriptions)
