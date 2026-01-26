f6479533562555ef008387b34217717da2f3e345
samueltwum1 <samueltwum1@gmail.com>
2026-01-26 14:02:23 +0000

    KAR-1923 Remove weather station

--- tests/acceptance/test_wms_attrs.py
+++ tests/acceptance/test_wms_attrs.py
@@ -4,6 +4,7 @@ import pytest
 from tango import AttrWriteType
 
 
+@pytest.mark.skip
 @pytest.mark.acceptance
 def test_wms_read_attribute_type(dish_manager_proxy) -> None:
     """Test the wms attribute configurations are read only."""
@@ -13,6 +14,7 @@ def test_wms_read_attribute_type(dish_manager_proxy) -> None:
     assert wind_speed_attribute_type == AttrWriteType.READ
 
 
+@pytest.mark.skip
 @pytest.mark.acceptance
 def test_wind_gust_updates(dish_manager_proxy, event_store_class):
     """Test that dish manager gets wind gust updates."""
