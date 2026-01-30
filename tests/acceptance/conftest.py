4d4de927a27605ae1398f1c0d7eb1335d107cb17
samueltwum1 <samueltwum1@gmail.com>
2026-01-30 18:20:58 +0000

    KAR-1923 Simplify reset fixture

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -13,8 +13,6 @@ from ska_mid_dish_manager.models.dish_enums import (
     DishMode,
     DSOperatingMode,
     DSPowerState,
-    SPFOperatingMode,
-    SPFRxOperatingMode,
 )
 from tests.utils import EventStore, remove_subscriptions, setup_subscriptions
 
@@ -69,81 +67,62 @@ def reset_dish_to_standby(
     spfrx_device_proxy,
 ):
     """Reset the tango devices to a fresh state before each test."""
-    event_store = event_store_class()
+    op_mode_events = event_store_class()
+    power_state_events = event_store_class()
     dish_mode_events = event_store_class()
-    result_events = event_store_class()
-    # this wait is very important for our AUTOMATED tests!!!
-    # wait for task status updates to finish before resetting the
-    # sub devices to a clean state for the next test. Reasons are:
-    # [*] your command map may never evaluate true for the
-    # awaited value to report the final task status of the LRC.
-    # [*] the base classes needs this final task status to allow the
-    # subsequently issued commands to be moved from queued to in progress
-    subs = setup_subscriptions(
-        dish_manager_proxy, {"longRunningCommandsInQueue": event_store}, reset_queue=False
-    )
-    try:
-        # it takes 10 seconds for the LRC marked as completed,
-        # aborted, failed or rejected to be removed
-        event_store.wait_for_value((), timeout=10)
-    except RuntimeError:
-        # carry on with the test even if the LRC is not cleared.
-        # the executor will pick up the next queued command since
-        # those commands from the previous test are not in progress.
-        pass
-    finally:
-        remove_subscriptions(subs)
+
+    lrc_status = dish_manager_proxy.longRunningCommandStatus
+    lrc_status = ", ".join([evt for evt in lrc_status])
+    if "IN_PROGRESS" in lrc_status:
+        # this wait is very important for our AUTOMATED tests!!!
+        # wait for task status updates to finish before resetting the
+        # sub devices to a clean state for the next test. Reasons are:
+        # [*] your command map may never evaluate true for the
+        # awaited value to report the final task status of the LRC.
+        # [*] the base classes needs this final task status to allow the
+        # subsequently issued commands to be moved from queued to in progress
+        op_mode_events.get_queue_events(timeout=5)
 
     subscriptions = {}
-    subscriptions.update(setup_subscriptions(spf_device_proxy, {"operatingMode": event_store}))
-    subscriptions.update(setup_subscriptions(spfrx_device_proxy, {"operatingMode": event_store}))
-    subscriptions.update(setup_subscriptions(ds_device_proxy, {"operatingMode": event_store}))
     subscriptions.update(
         setup_subscriptions(
-            dish_manager_proxy,
-            {
-                "dishMode": dish_mode_events,
-                "longRunningCommandResult": result_events,
-                "dscCmdAuth": event_store,
-            },
+            ds_device_proxy,
+            {"operatingMode": op_mode_events, "powerState": power_state_events},
         )
     )
+    subscriptions.update(setup_subscriptions(dish_manager_proxy, {"dishMode": dish_mode_events}))
+
+    # reset the simulated devices
+    spfrx_device_proxy.ResetToDefault()
+    spf_device_proxy.ResetToDefault()
 
     try:
         if dish_manager_proxy.dishMode == DishMode.MAINTENANCE:
             dish_manager_proxy.SetStowMode()
-            dish_mode_events.wait_for_value(DishMode.STOW, timeout=10)
+            dish_mode_events.wait_for_value(DishMode.STOW, timeout=120)
 
-        if dish_manager_proxy.dishmode != DishMode.STANDBY_LP:
-            spf_device_proxy.ResetToDefault()
-            assert event_store.wait_for_value(SPFOperatingMode.STANDBY_LP, timeout=10)
+        if ds_device_proxy.operatingMode != DSOperatingMode.STANDBY:
+            ds_device_proxy.SetStandbyMode()
+            op_mode_events.wait_for_value(DSOperatingMode.STANDBY, timeout=10)
+            power_state_events.wait_for_value(DSPowerState.LOW_POWER, timeout=10)
+        # go to FP
+        ds_device_proxy.SetPowerMode([0.0, 14.7])
+        power_state_events.wait_for_value(DSPowerState.FULL_POWER, timeout=10)
 
-            spfrx_device_proxy.ResetToDefault()
-            assert event_store.wait_for_value(SPFRxOperatingMode.STANDBY, timeout=10)
-
-            if (
-                ds_device_proxy.operatingMode != DSOperatingMode.STANDBY
-                or ds_device_proxy.powerstate != DSPowerState.LOW_POWER
-            ):
-                # go to LP ...
-                ds_device_proxy.SetStandbyMode()
-            dish_mode_events.wait_for_value(DishMode.STANDBY_LP, timeout=30)
     except (RuntimeError, AssertionError):
-        # check dish manager before giving up
-        logger.exception("Failed to reset subdevices to known states.")
-        logger.info("DishManager commands: %s", dish_manager_proxy.longrunningcommandstatus)
-        logger.info("DSManager commands: %s", dish_manager_proxy.longrunningcommandstatus)
+        pass
 
-    try:
-        if dish_manager_proxy.dishmode != DishMode.STANDBY_FP:
-            [[_], [unique_id]] = dish_manager_proxy.SetStandbyFPMode()
-            result_events.wait_for_command_id(unique_id, timeout=30)
-            dish_mode_events.wait_for_value(DishMode.STANDBY_FP, timeout=30)
-    except RuntimeError:
-        component_states = dish_manager_proxy.GetComponentStates()
-        dish_mode = dish_manager_proxy.read_attribute("dishMode").value
-        assert dish_mode == DishMode.STANDBY_FP, component_states
-    finally:
-        remove_subscriptions(subscriptions)
+    if dish_manager_proxy.dishMode != DishMode.STANDBY_FP:
+        try:
+            dish_manager_proxy.SetStandbyFPMode()
+            dish_mode_events.wait_for_value(DishMode.STANDBY_FP, timeout=10)
+        except RuntimeError:
+            logger.debug("DishManager commands: %s", dish_manager_proxy.longrunningcommandstatus)
+            logger.debug("DSManager commands: %s", dish_manager_proxy.longrunningcommandstatus)
+            logger.debug("\n\nDM component state: %s\n\n", dish_manager_proxy.GetComponentStates())
+            remove_subscriptions(subscriptions)
+            raise
+
+    remove_subscriptions(subscriptions)
 
     yield
