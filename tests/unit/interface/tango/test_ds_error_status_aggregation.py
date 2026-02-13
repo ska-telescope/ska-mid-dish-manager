2c2a0c1d9100f24d694a36ae121a49f4530d4634
DevonPetrie <devonpetrie.me@gmail.com>
2026-02-13 11:31:13 +0200

    KAR-2068 expand error status aggregation test

--- tests/unit/interface/tango/test_ds_error_status_aggregation.py
+++ tests/unit/interface/tango/test_ds_error_status_aggregation.py
@@ -3,6 +3,8 @@
 import pytest
 import tango
 
+from ska_mid_dish_manager.models.constants import DS_ERROR_STATUS_ATTRIBUTES
+
 
 @pytest.mark.unit
 @pytest.mark.forked
@@ -22,13 +24,45 @@ def test_ds_error_status_aggregation(
         event_store,
     )
 
+    def clear_errors():
+        """Set all error status attributes to False."""
+        ds_cm._update_component_state(**{key.lower(): False for key in DS_ERROR_STATUS_ATTRIBUTES})
+        event_store.wait_for_value("OK")
+
+    # Test individual errors
+    for attr, message in DS_ERROR_STATUS_ATTRIBUTES.items():
+        clear_errors()
+        # Trigger single error
+        ds_cm._update_component_state(**{attr.lower(): True})
+        event_store.wait_for_value(message)
+
+    # Test multiple errors
+    clear_errors()
+
+    error_subset = list(DS_ERROR_STATUS_ATTRIBUTES.keys())[:3]
+
+    ds_cm._update_component_state(**{key.lower(): True for key in error_subset})
+    expected = "; ".join(DS_ERROR_STATUS_ATTRIBUTES[key] for key in error_subset)
+    event_store.wait_for_value(expected)
+
+    # Mixed on/off
+    clear_errors()
+
+    first, second = error_subset[:2]
+
+    # First True
+    ds_cm._update_component_state(**{first.lower(): True})
+    event_store.wait_for_value(DS_ERROR_STATUS_ATTRIBUTES[first])
+
+    # Second True
+    ds_cm._update_component_state(**{second.lower(): True})
+    expected = "; ".join(DS_ERROR_STATUS_ATTRIBUTES[key] for key in error_subset[:2])
+    event_store.wait_for_value(expected)
+
+    # First False
+    ds_cm._update_component_state(**{first.lower(): False})
+    event_store.wait_for_value(DS_ERROR_STATUS_ATTRIBUTES[second])
+
+    # Second False
+    ds_cm._update_component_state(**{second.lower(): False})
     event_store.wait_for_value("OK")
-
-    # Mimic errors from DSC
-    ds_cm._update_component_state(errazimuth=True)
-    event_store.wait_for_value("Azimuth Axis error")
-
-    ds_cm._update_component_state(errpwr600vdc=True, errstwpin=True)
-    event_store.wait_for_value(
-        "Azimuth Axis error; Power error on 600 VDC; StowPin Controller error"
-    )
