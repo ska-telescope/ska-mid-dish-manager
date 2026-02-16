d62ee14783727888692c08b5ba29e35aaceef0f7
Barbara O <bojur@sarao.ac.za>
2026-02-16 06:42:30 +0200

    KAR-2094 included unsaved file

--- tests/acceptance/transitions/test_capability_state.py
+++ tests/acceptance/transitions/test_capability_state.py
@@ -1,10 +1,7 @@
 """Test CapabilityState."""
 
 import pytest
-from ska_mid_dish_simulators.sim_enums import (
-    Band,
-)
-from ska_mid_dish_utils.models.dish_enums import CapabilityStates, DishMode
+from ska_mid_dish_utils.models.dish_enums import Band, CapabilityStates, DishMode
 
 from tests.utils import remove_subscriptions, setup_subscriptions
 
