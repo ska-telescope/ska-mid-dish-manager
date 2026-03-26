554141b70672ec64eefd52ffee1134c67ddd1c72
silere8 <usilere@sarao.ac.za>
2026-03-26 14:30:42 +0200

    KAR-2105 Added tests, marked as XFAIL, command logic is still WIP

---
+++ tests/acceptance/test_reconnection_command.py
@@ -0,0 +1,48 @@
+"""Test reset connection command."""
+
+from typing import Any
+
+import pytest
+import tango
+from ska_control_model import CommunicationStatus
+
+from tests.utils import remove_subscriptions, setup_subscriptions
+
+
+@pytest.mark.acceptance
+@pytest.mark.xfail(reason="Still working on the command logic")
+def test_reset_connection_cmd(
+    dish_manager_proxy: tango.DeviceProxy, event_store_class: Any
+) -> None:
+    """Test reseting of connection command."""
+    ds_connection_state_event_store = event_store_class()
+    spf_connection_state_event_store = event_store_class()
+    spfrx_connection_state_event_store = event_store_class()
+
+    attr_cb_mapping = {
+        "dsConnectionState": ds_connection_state_event_store,
+        "spfConnectionState": spf_connection_state_event_store,
+        "spfrxConnectionState": spfrx_connection_state_event_store,
+    }
+
+    subscriptions = setup_subscriptions(dish_manager_proxy, attr_cb_mapping)
+
+    ds_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=6)
+    spf_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=6)
+    spfrx_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=6)
+
+    dish_manager_proxy.ResetConnections(["DS", "SPF", "SPFRX"])
+
+    ds_connection_state_event_store.wait_for_value(CommunicationStatus.NOT_ESTABLISHED, timeout=30)
+    spf_connection_state_event_store.wait_for_value(
+        CommunicationStatus.NOT_ESTABLISHED, timeout=30
+    )
+    spfrx_connection_state_event_store.wait_for_value(
+        CommunicationStatus.NOT_ESTABLISHED, timeout=30
+    )
+
+    ds_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=60)
+    spf_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=60)
+    spfrx_connection_state_event_store.wait_for_value(CommunicationStatus.ESTABLISHED, timeout=60)
+
+    remove_subscriptions(subscriptions)
---
+++ tests/unit/interface/tango/test_reconnection_cmd.py
@@ -0,0 +1,100 @@
+"""Test that the DishManager ResetSubsconnection attribute."""
+
+import pytest
+from ska_control_mode import ResultCode
+
+
+@pytest.mark.unit
+@pytest.mark.xfail(reason="Still working on the command logic")
+@pytest.mark.parametrize(
+    "device_names",
+    "expected_output",
+    [
+        (
+            ["SD", "PFS", "SRRFRX"],
+            (ResultCode.FAILED, "Invalid device name: SD. Valid device names are: DS, SPF, SPFRX"),
+        ),
+        (
+            ["111"],
+            (
+                ResultCode.FAILED,
+                "Invalid device name: 111. Valid device names are: DS, SPF, SPFRX",
+            ),
+        ),
+        (
+            ["SPFC"],
+            (
+                ResultCode.FAILED,
+                "Invalid device name: SPFC. Valid device names are: DS, SPF, SPFRX",
+            ),
+        ),
+        (
+            ["SPFRx"],
+            (
+                ResultCode.FAILED,
+                "Invalid device name: SPFRx. Valid device names are: DS, SPF, SPFRX",
+            ),
+        ),
+        (
+            ["INVALID_DEVICE"],
+            (
+                ResultCode.FAILED,
+                "Invalid device name: INVALID_DEVICE. Valid device names are: DS, SPF, SPFRX",
+            ),
+        ),
+    ],
+)
+def test_reset_subs_connection_invalid_device_names(
+    dish_manager_resources, device_names, expected_output, event_store_class
+):
+    """Test that ResetSubsConnection command raises an
+    exception when invalid device names are passed.
+    """
+    device_proxy, dish_manager_cm = dish_manager_resources
+    result = device_proxy.ResetSubsConnections(device_names)
+    assert result == expected_output
+
+
+@pytest.mark.unit
+@pytest.mark.xfail(reason="Still working on the command logic")
+def test_reset_subs_connection_valid_device_names(dish_manager_resources, event_store_class):
+    """Test that ResetSubsConnection command resets the connections
+    when valid device names are passed.
+    """
+    device_proxy, dish_manager_cm = dish_manager_resources
+    result = device_proxy.ResetSubsConnections(["DS", "SPF", "SPFRX"])
+    assert result == (ResultCode.OK, "Re-connection(s) have been intiated successfully")
+
+
+@pytest.mark.unit
+@pytest.mark.xfail(reason="Still working on the command logic")
+def test_reset_subs_connection_no_device_names_input(dish_manager_resources, event_store_class):
+    """Test that ResetSubsConnection command raises an exception
+    when no device names are passed.
+    """
+    device_proxy, dish_manager_cm = dish_manager_resources
+    result = device_proxy.ResetSubsConnections([])
+    assert result == (ValueError, "[device_names] cannot be an empty list")
+
+
+@pytest.mark.unit
+@pytest.mark.xfail(reason="Still working on the command logic")
+@pytest.mark.parametrize(
+    "device_name, expected_result",
+    [
+        ("SPF", (ValueError, "Reconnection denied, device SPF is ignored")),
+        ("SPFRX", (ValueError, "Reconnection denied, device SPFRX is ignored")),
+        ("B5DC", (ValueError, "Reconnection denied, B5DC device is not monitored")),
+    ],
+)
+def test_reset_subs_connection_ignored_devices(
+    dish_manager_resources, event_store_class, device_name, expected_result
+):
+    """Test that ResetSubsConnection command raises an exception
+    when a sub device is ignored.
+    """
+    device_proxy, dish_manager_cm = dish_manager_resources
+    device_proxy.ignorespf = True
+    device_proxy.ignorespfrx = True
+    result = device_proxy.ResetSubsConnections([device_name])
+    assert result == expected_result
