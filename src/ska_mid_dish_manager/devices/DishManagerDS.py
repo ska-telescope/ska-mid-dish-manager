798d081229d015b8e1f0b59068da148ad2f57951
Yashren Reddi <yreddi@sarao.ac.za>
2026-03-26 11:28:12 +0200

    KAR-2109: Prototype log tag override

--- src/ska_mid_dish_manager/devices/DishManagerDS.py
+++ src/ska_mid_dish_manager/devices/DishManagerDS.py
@@ -122,6 +122,7 @@ class DishManager(SKAController):
         :return: Instance of DishManagerComponentManager
         :rtype: DishManagerComponentManager
         """
+        self.logger.info("Yash testing stages.", extra=OPERATOR_TAG)
         return DishManagerComponentManager(
             self.logger,
             self._command_tracker,
@@ -519,8 +520,8 @@ class DishManager(SKAController):
 
             device.instances[device.get_name()] = device
             (result_code, message) = super().do()
-            self._configure_additional_user_tags_for_logging()
-            self.logger.info("Device initialized successfully.", extra=OPERATOR_TAG)
+            device._configure_additional_user_tags_for_logging()
+            device.logger.info("Device initialized successfully.", extra=OPERATOR_TAG)
             device.op_state_model.perform_action("component_on")
             device.component_manager.start_communicating()
             return (ResultCode(result_code), message)
