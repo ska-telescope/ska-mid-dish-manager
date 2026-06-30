b7388a1c8b8326d749bc6b50b152d3228676dabc
Devon Petrie <devonpetrie.me@gmail.com>
2026-06-30 12:10:07 +0200

    KAR-2278 Update tests

--- tests/unit/interface/no_tango/test_cm_configureband.py
+++ tests/unit/interface/no_tango/test_cm_configureband.py
@@ -83,9 +83,11 @@ def test_configureband_handler(
     task_cb = callbacks["task_cb"]
     task_cb.assert_called_with(
         status=TaskStatus.COMPLETED,
-        result=(ResultCode.OK, "SetOperateMode completed."),
+        result=(ResultCode.OK, "DS and SPF already in operate state, SetOperateMode completed"),
     )
-    progress_cb.wait_for_args(("SetOperateMode completed.",))
+    progress_cb.wait_for_args(
+        ("DS and SPF already in operate state, SetOperateMode completed",)
+    )
 
     # Verify apply_pointing_model was called
     mock_apply_pointing_model.assert_called_once()
@@ -173,9 +175,11 @@ def test_configureband_json_handler_happy(
     task_cb = callbacks["task_cb"]
     task_cb.assert_called_with(
         status=TaskStatus.COMPLETED,
-        result=(ResultCode.OK, "SetOperateMode completed."),
+        result=(ResultCode.OK, "DS and SPF already in operate state, SetOperateMode completed"),
     )
-    progress_cb.wait_for_args(("SetOperateMode completed.",))
+    progress_cb.wait_for_args(
+        ("DS and SPF already in operate state, SetOperateMode completed",)
+    )
 
     # Verify apply_pointing_model was called
     mock_apply_pointing_model.assert_called_once()
@@ -424,12 +428,21 @@ def test_configureband_5b_with_subband(
     task_cb = callbacks["task_cb"]
     task_cb.assert_called_with(
         status=TaskStatus.COMPLETED,
-        result=(ResultCode.OK, "SetOperateMode completed."),
+        result=(ResultCode.OK, "DS and SPF already in operate state, SetOperateMode completed"),
     )
-    progress_cb.wait_for_args(("SetOperateMode completed.",))
+    progress_cb.wait_for_args(
+        ("DS and SPF already in operate state, SetOperateMode completed",)
+    )
 
 
+@pytest.mark.unit
+@patch(
+    "ska_mid_dish_manager.models.dish_mode_model.DishModeModel.is_command_allowed",
+    Mock(return_value=True),
+)
+@patch("ska_mid_dish_manager.models.command_actions.apply_pointing_model")
 def test_configureband_5b_with_subband_ignore_b5dc(
+    mock_apply_pointing_model,
     component_manager: DishManagerComponentManager,
     callbacks: dict,
 ) -> None:
@@ -456,6 +469,8 @@ def test_configureband_5b_with_subband_ignore_b5dc(
             ] }
     }"""
     component_manager._component_state["ignoreb5dc"] = True
+    component_manager.component_state["band2pointingmodelparams"] = [1.8] * 18
+    mock_apply_pointing_model.return_value = None
     status, response = component_manager.configure_band_with_json(
         configure_json, callbacks["task_cb"]
     )
@@ -478,8 +493,8 @@ def test_configureband_5b_with_subband_ignore_b5dc(
         "Awaiting DS indexerposition change to B5b",
         "Awaiting SPFRX configuredband change to B1",
         "Fanned out commands: DS.SetIndexPosition, SPFRX.ConfigureBand",
-        "B5DC device is disabled. B5DC.SetFrequency ignored",
         "Awaiting configuredband change to B5b",
+        "B5DC.SetFrequency ignored",
     ]
     progress_cb = callbacks["progress_cb"]
     for msg in msgs:
@@ -503,9 +518,11 @@ def test_configureband_5b_with_subband_ignore_b5dc(
     task_cb = callbacks["task_cb"]
     task_cb.assert_called_with(
         status=TaskStatus.COMPLETED,
-        result=(ResultCode.OK, "SetOperateMode completed"),
+        result=(ResultCode.OK, "DS and SPF already in operate state, SetOperateMode completed"),
     )
-    progress_cb.wait_for_args(("SetOperateMode completed",))
+    progress_cb.wait_for_args(
+        ("DS and SPF already in operate state, SetOperateMode completed",)
+    )
 
 
 @pytest.mark.unit
