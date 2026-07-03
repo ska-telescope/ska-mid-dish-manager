bb690f9c48f8363c05085af77d818b725e1f459e
Mpumelelo Nzama <mnzama@sarao.ac.za>
2026-07-03 11:14:48 +0200

    KAR-2273 Updated expected abort result

--- tests/acceptance/transitions/test_abort_commands.py
+++ tests/acceptance/transitions/test_abort_commands.py
@@ -57,7 +57,7 @@ def test_abort_commands(
     [[_], [unique_id]] = dish_manager_proxy.Abort()
     # Confirm Dish Manager aborted the request on the Configure action
     result_event_store.wait_for_finished_command_result(
-        unique_id, '[0, "Abort sequence completed"]', timeout=30
+        unique_id, "[0, 'Abort completed OK']", timeout=30
     )
     status_event_store.wait_for_progress_update("SetOperateMode aborted", timeout=30)
 
@@ -162,7 +162,7 @@ def test_abort_commands_during_track(
     # Call Abort on DishManager
     [[_], [unique_id]] = dish_manager_proxy.Abort()
     result_event_store.wait_for_finished_command_result(
-        unique_id, '[0, "Abort sequence completed"]', timeout=30
+        unique_id, "[0, 'Abort completed OK']", timeout=30
     )
 
     main_event_store.wait_for_value(DishMode.STANDBY_FP, timeout=10)
@@ -201,7 +201,7 @@ def test_abort_commands_during_slew(
     # Call Abort on DishManager
     [[_], [unique_id]] = dish_manager_proxy.Abort()
     result_event_store.wait_for_finished_command_result(
-        unique_id, '[0, "Abort sequence completed"]', timeout=30
+        unique_id, "[0, 'Abort completed OK']", timeout=30
     )
 
     main_event_store.wait_for_value(DishMode.STANDBY_FP, timeout=30)
@@ -250,7 +250,7 @@ def test_abort_commands_during_stow(
     [[_], [unique_id]] = dish_manager_proxy.Abort()
     print([[_], [unique_id]])
     result_event_store.wait_for_finished_command_result(
-        unique_id, '[0, "Abort sequence completed"]', timeout=30
+        unique_id, "[0, 'Abort completed OK']", timeout=30
     )
 
     main_event_store.wait_for_value(DishMode.STANDBY_FP, timeout=30)
