276f2f8ec173045d2f10a69d4fc7737c985731ad
Barbara Ojur <barbsapili@gmail.com>
2026-07-15 13:46:33 +0000

    KAR-2279 Healthinfo dim_x Error

--- tests/acceptance/test_healthinfo.py
+++ tests/acceptance/test_healthinfo.py
@@ -4,20 +4,32 @@ import pytest
 import tango
 from ska_control_model import HealthState
 
+from ska_mid_dish_manager.models.dish_enums import SPFHealthState
+
 
 @pytest.mark.acceptance
-def test_healthinfo(dish_manager_proxy, spfrx_device_proxy, event_store_class):
+def test_healthinfo(dish_manager_proxy, spfrx_device_proxy, spf_device_proxy, event_store_class):
     """Test the healthInfo attribute."""
     event_store = event_store_class()
     sub_id = dish_manager_proxy.subscribe_event(
         "healthInfo", tango.EventType.CHANGE_EVENT, event_store
     )
     spfrx_device_proxy.write_attribute("healthState", HealthState.OK)
+    spf_device_proxy.write_attribute("healthState", SPFHealthState.NORMAL)
+
     event_store.wait_for_value((), timeout=10)
     spfrx_device_proxy.write_attribute("healthState", HealthState.FAILED)
+    # Added to ensure that healthInfo doesn't throw a data size error.
+    spf_device_proxy.write_attribute("healthState", SPFHealthState.DEGRADED)
+
     event_store.wait_for_value(
-        ('mid-dish/simulator-spfrx/SKA001: ["Unknown failure reason"]',), timeout=10
+        (
+            'mid-dish/simulator-spfc/SKA001: ["Unknown degraded reason"]',
+            'mid-dish/simulator-spfrx/SKA001: ["Unknown failure reason"]',
+        ),
+        timeout=10,
     )
+    spf_device_proxy.write_attribute("healthState", SPFHealthState.NORMAL)
     spfrx_device_proxy.write_attribute("healthState", HealthState.DEGRADED)
     event_store.wait_for_value(
         ('mid-dish/simulator-spfrx/SKA001: ["Unknown degraded reason"]',), timeout=10
